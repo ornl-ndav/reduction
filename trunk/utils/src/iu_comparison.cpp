7606747ce12280bf55cd62c2b0d7168e4acc20df
j35 <j35@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2005-12-14 22:33:40 +0000

    Functions used by the test files

---
+++ trunk/utils/src/iu_comparison.cpp
@@ -0,0 +1,47 @@
+#include "num_comparison.hpp"
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+using namespace std;
+
+namespace Utils
+{
+  template <typename NumT>
+  void
+  iu_comparison (std::vector<NumT> const & value,
+		 std::vector<NumT> const & true_value,
+		 int & error,
+		 const int add_error,
+		 const int n_max)
+  {
+    for (int i=0; i < n_max; i++)
+      {
+	if (value[i] != true_value[i])
+	  {
+	    error += add_error;
+	    break;
+	  }
+      }
+    return;
+  }
+
+  // explicit instantiations
+
+  // This is the int declaration
+  template void 
+  iu_comparison (std::vector<int> const & value,
+		 std::vector<int> const & true_value,
+		 int & error,
+		 const int add_error,
+		 const int n_max);
+  
+  template void 
+  iu_comparison (std::vector<unsigned int> const & value,
+		 std::vector<unsigned int> const & true_value,
+		 int & error,
+		 const int add_error,
+		 const int n_max);
+  
+}
