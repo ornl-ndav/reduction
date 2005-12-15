c17bbb1c4df8d9533e5ff578b18bd71eddd3a178
j35 <j35@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2005-12-15 21:11:10 +0000

    New test code init_scatt_wavector_to_scalar_Q_test

--- trunk/axis_manip/test/cpp/init_scatt_wavevector_to_scalar_Q_test.cpp
+++ trunk/axis_manip/test/cpp/init_scatt_wavevector_to_scalar_Q_test.cpp
@@ -115,23 +115,23 @@ int main()
   
   //initialization of parameters
 
-  f_polar_angle = static_cast<float>(34.45);
-  d_polar_angle = static_cast<double>(34.45);
-  i_polar_angle = static_cast<int>(34.45);
-  u_polar_angle = static_cast<unsigned int>(34.45);
+  f_polar_angle = 34.45;
+  d_polar_angle = 34.45;
+  i_polar_angle = (int)(34.45);
+  u_polar_angle = (unsigned int)(34.45);
 
-  f_polar_angle_err2 = static_cast<float>(34.45);
-  d_polar_angle_err2 = static_cast<double>(34.45);
-  i_polar_angle_err2 = static_cast<int>(34.45);
-  u_polar_angle_err2 = static_cast<unsigned int>(34.45);
+  f_polar_angle_err2 = 34.45;
+  d_polar_angle_err2 = 34.45;
+  i_polar_angle_err2 = (int)34.45;
+  u_polar_angle_err2 = (unsigned int)(34.45);
 
   //case 1
 
-      f_initial_wavevector.push_back(2*(float)3.5);
-      f_initial_wavevector_err2.push_back((float)4.4);
+      f_initial_wavevector.push_back(2*3.5);
+      f_initial_wavevector_err2.push_back(4.4);
 
-      d_initial_wavevector.push_back(2*(double)3.5);
-      d_initial_wavevector_err2.push_back((double)4.4);
+      d_initial_wavevector.push_back(2*3.5);
+      d_initial_wavevector_err2.push_back(4.4);
 
       i_initial_wavevector.push_back(2*(int)3.5);
       i_initial_wavevector_err2.push_back((int)4.4);
@@ -141,23 +141,23 @@ int main()
 
   for(int i=0; i<n; i++)            
     {
-      f_final_wavevector.push_back(2*(float)i+1);
-      f_final_wavevector_err2.push_back(2*(float)i);
+      f_final_wavevector.push_back(2*i+1);
+      f_final_wavevector_err2.push_back(2*i);
       
-      d_final_wavevector.push_back(2*(double)i+1);
-      d_final_wavevector_err2.push_back(2*(double)i);
+      d_final_wavevector.push_back(2*i+1);
+      d_final_wavevector_err2.push_back(2*i);
 
-      i_final_wavevector.push_back(2*(int)i+1);
-      i_final_wavevector_err2.push_back(2*(int)i);
+      i_final_wavevector.push_back(2*i+1);
+      i_final_wavevector_err2.push_back(2*i);
 
-      u_final_wavevector.push_back(2*(unsigned int)i+1);
-      u_final_wavevector_err2.push_back(2*(unsigned int)i);
+      u_final_wavevector.push_back(2*i+1);
+      u_final_wavevector_err2.push_back(2*i);
     }
 
   AxisManip::init_scatt_wavevector_to_scalar_Q(f_initial_wavevector, f_initial_wavevector_err2,
 					       f_final_wavevector, f_final_wavevector_err2,
 					       f_polar_angle, f_polar_angle_err2, f_Q, f_Q_err2);
-
+  
   AxisManip::init_scatt_wavevector_to_scalar_Q(d_initial_wavevector, d_initial_wavevector_err2,
 					       d_final_wavevector, d_final_wavevector_err2,
 					       d_polar_angle, d_polar_angle_err2, d_Q, d_Q_err2);
@@ -176,13 +176,13 @@ int main()
 						   0,f_final_wavevector, f_final_wavevector_err2,
 						   i,f_polar_angle,f_polar_angle_err2,
 						   f_true_Q, f_true_Q_err2, i);
-
+      
       d_function_init_scatt_wavevector_to_scalar_Q(d_initial_wavevector, d_initial_wavevector_err2,
 						   0,d_final_wavevector, d_final_wavevector_err2,
 						   i,d_polar_angle,d_polar_angle_err2,
 						   d_true_Q, d_true_Q_err2, i);
 
-      i_function_init_scatt_wavevector_to_scalar_Q(i_initial_wavevector, i_initial_wavevector_err2,
+        i_function_init_scatt_wavevector_to_scalar_Q(i_initial_wavevector, i_initial_wavevector_err2,
 						   0,i_final_wavevector, i_final_wavevector_err2,
 						   i,i_polar_angle,i_polar_angle_err2,
 						   i_true_Q, i_true_Q_err2, i);
@@ -237,29 +237,29 @@ int main()
   u_initial_wavevector.clear();
   u_initial_wavevector_err2.clear();
 
-  f_final_wavevector.push_back(static_cast<float>(14.34));
-  d_final_wavevector.push_back(static_cast<double>(14.34));
-  i_final_wavevector.push_back(static_cast<int>(14.34));
-  u_final_wavevector.push_back(static_cast<unsigned int>(14.34));
+  f_final_wavevector.push_back(14.34);
+  d_final_wavevector.push_back(14.34);
+  i_final_wavevector.push_back((int)14.34);
+  u_final_wavevector.push_back((unsigned int)14.34);
 
-  f_final_wavevector_err2.push_back(static_cast<float>(4.34));
-  d_final_wavevector_err2.push_back(static_cast<double>(4.34));
-  i_final_wavevector_err2.push_back(static_cast<int>(4.34));
-  u_final_wavevector_err2.push_back(static_cast<unsigned int>(4.34));
+  f_final_wavevector_err2.push_back(4.34);
+  d_final_wavevector_err2.push_back(4.34);
+  i_final_wavevector_err2.push_back((int)4.34);
+  u_final_wavevector_err2.push_back((unsigned int)4.34);
   
   for(int i=0; i<n; i++)            
     {
-      f_initial_wavevector.push_back((float)i+1);
-      f_initial_wavevector_err2.push_back((float)i);
+      f_initial_wavevector.push_back(i+1);
+      f_initial_wavevector_err2.push_back(i);
       
-      d_initial_wavevector.push_back((double)i+1);
-      d_initial_wavevector_err2.push_back((double)i);
+      d_initial_wavevector.push_back(i+1);
+      d_initial_wavevector_err2.push_back(i);
 
-      i_initial_wavevector.push_back((int)i+1);
-      i_initial_wavevector_err2.push_back((int)i);
+      i_initial_wavevector.push_back(i+1);
+      i_initial_wavevector_err2.push_back(i);
 
-      u_initial_wavevector.push_back((unsigned int)i+1);
-      u_initial_wavevector_err2.push_back((unsigned int)i);
+      u_initial_wavevector.push_back(i+1);
+      u_initial_wavevector_err2.push_back(i);
     }
 
   AxisManip::init_scatt_wavevector_to_scalar_Q(f_initial_wavevector, f_initial_wavevector_err2,
@@ -345,21 +345,21 @@ int main()
   u_initial_wavevector.clear();
   u_initial_wavevector_err2.clear();
 
-  f_final_wavevector.push_back((float)(14.34));
-  d_final_wavevector.push_back((double)(14.34));
+  f_final_wavevector.push_back((14.34));
+  d_final_wavevector.push_back((14.34));
   i_final_wavevector.push_back((int)(14.34));
   u_final_wavevector.push_back((unsigned int)(14.34));
 
-  f_final_wavevector_err2.push_back((float)(4.34));
-  d_final_wavevector_err2.push_back((double)(4.34));
+  f_final_wavevector_err2.push_back((4.34));
+  d_final_wavevector_err2.push_back((4.34));
   i_final_wavevector_err2.push_back((int)(4.34));
   u_final_wavevector_err2.push_back((unsigned int)(4.34));
   
-  f_initial_wavevector.push_back((float)11.44);
-  f_initial_wavevector_err2.push_back((float)2.2);
+  f_initial_wavevector.push_back(11.44);
+  f_initial_wavevector_err2.push_back(2.2);
   
-  d_initial_wavevector.push_back((double)11.44);
-  d_initial_wavevector_err2.push_back((double)2.2);
+  d_initial_wavevector.push_back(11.44);
+  d_initial_wavevector_err2.push_back(2.2);
   
   i_initial_wavevector.push_back((int)11.44);
   i_initial_wavevector_err2.push_back((int)2.2);
@@ -451,15 +451,15 @@ int main()
 
   for(int i=0; i<n; i++)            
     {
-      f_initial_wavevector.push_back((float)i+1);
-      f_initial_wavevector_err2.push_back((float)i);
-      f_final_wavevector.push_back(2*(float)i+3);
-      f_final_wavevector_err2.push_back((float)i+2);
+      f_initial_wavevector.push_back(i+1);
+      f_initial_wavevector_err2.push_back(i);
+      f_final_wavevector.push_back(2*i+3);
+      f_final_wavevector_err2.push_back(i+2);
       
-      d_initial_wavevector.push_back((double)i+1);
-      d_initial_wavevector_err2.push_back((double)i);
-      d_final_wavevector.push_back(2*(double)i+3);
-      d_final_wavevector_err2.push_back((double)i+2);
+      d_initial_wavevector.push_back(i+1);
+      d_initial_wavevector_err2.push_back(i);
+      d_final_wavevector.push_back(2*i+3);
+      d_final_wavevector_err2.push_back(i+2);
 
       i_initial_wavevector.push_back((int)i+1);
       i_initial_wavevector_err2.push_back((int)i);
@@ -690,16 +690,18 @@ void f_function_init_scatt_wavevector_to_scalar_Q(vector<float> & f_initial_wave
 						  int k)
 {
   float f_a;
-  float f_pang;
+  float f_pang, f_sang;
   float f_ki2, f_kf2, f_akikf;
   float f_termi, f_termi2;
   float f_termf, f_termf2;
   float f_front;
+  float f_polar, f_polar2;
 
   f_ki2 = f_initial_wavevector[i] * f_initial_wavevector[i];
 
   f_a = static_cast<float>(2 * std::cos(static_cast<double>(f_polar_angle)));
   f_pang = static_cast<float>(std::cos(static_cast<double>(f_polar_angle)));
+  f_sang = static_cast<float>(std::sin(static_cast<double>(f_polar_angle)));
 
   f_kf2 = f_final_wavevector[j] * f_final_wavevector[j];
   f_akikf = f_a * f_initial_wavevector[i] * f_final_wavevector[j];
@@ -708,13 +710,22 @@ void f_function_init_scatt_wavevector_to_scalar_Q(vector<float> & f_initial_wave
   
   f_termi = f_initial_wavevector[i] - (f_final_wavevector[j] * f_pang);
   f_termi2 = f_termi * f_termi;
-  f_termf = f_final_wavevector[j] - (f_initial_wavevector[i] * f_pang);
+
+  f_termf = f_initial_wavevector[i] * f_pang;
+  f_termf = f_final_wavevector[j] - f_termf;
   f_termf2 = f_termf * f_termf;
-  f_front = 4. / (f_true_Q[k] * f_true_Q[k]);
+
+  f_polar = f_final_wavevector[j] * f_initial_wavevector[i];
+  f_polar = f_polar * f_sang;
+  f_polar2 = f_polar * f_polar;
+
+  f_front = 1 / (f_true_Q[k] * f_true_Q[k]);
   
-  f_true_Q_err2[k] = f_front * (( f_initial_wavevector_err2[i] * f_termi2) +
-				( f_final_wavevector_err2[j] * f_termf2));
-  
+  f_true_Q_err2[k] = f_final_wavevector_err2[j] * f_termf2;
+  f_true_Q_err2[k] = f_true_Q_err2[k] + f_initial_wavevector_err2[i] * f_termi2;
+  f_true_Q_err2[k] = f_true_Q_err2[k] + (f_polar_angle_err2 * f_polar2);
+  f_true_Q_err2[k] = f_true_Q_err2[k] * f_front ;
+
   return;
 }
 
@@ -732,31 +743,44 @@ void d_function_init_scatt_wavevector_to_scalar_Q(vector<double> & d_initial_wav
 						  int k)
 {
   double d_a;
-  double d_pang;
+  double d_pang, d_sang;
   double d_ki2, d_kf2, d_akikf;
   double d_termi, d_termi2;
   double d_termf, d_termf2;
   double d_front;
+  double d_polar, d_polar2;
 
   d_a = static_cast<double>(2 * std::cos(static_cast<double>(d_polar_angle)));
   d_pang = static_cast<double>(std::cos(static_cast<double>(d_polar_angle)));
+  d_sang = static_cast<double>(std::sin(static_cast<double>(d_polar_angle)));
 
   d_ki2 = d_initial_wavevector[i] * d_initial_wavevector[i];
 
   d_kf2 = d_final_wavevector[j] * d_final_wavevector[j];
-  d_akikf = d_a * d_initial_wavevector[i] * d_final_wavevector[j];
+  d_akikf = d_a * d_initial_wavevector[i];
+  d_akikf = d_akikf * d_final_wavevector[j];
   
   d_true_Q[k] = static_cast<double>(std::sqrt(static_cast<double>(d_ki2 + d_kf2 - d_akikf)));
   
-  d_termi = d_initial_wavevector[i] - (d_final_wavevector[j] * d_pang);
+  d_termi = d_final_wavevector[j] * d_pang;
+  d_termi = d_initial_wavevector[i] - d_termi;
   d_termi2 = d_termi * d_termi;
-  d_termf = d_final_wavevector[j] - (d_initial_wavevector[i] * d_pang);
+
+  d_termf = d_initial_wavevector[i] * d_pang;
+  d_termf = d_final_wavevector[j] - d_termf;
   d_termf2 = d_termf * d_termf;
-  d_front = 4 / (d_true_Q[k] * d_true_Q[k]);
+
+  d_polar = d_final_wavevector[j] * d_initial_wavevector[i] ;
+  d_polar = d_polar *  d_sang;
+  d_polar2 = d_polar * d_polar;
+
+  d_front = 1 / (d_true_Q[k] * d_true_Q[k]);
   
-  d_true_Q_err2[k] = d_front * (( d_initial_wavevector_err2[i] * d_termi2) +
-				( d_final_wavevector_err2[j] * d_termf2));
-  
+  d_true_Q_err2[k] = d_initial_wavevector_err2[i] * d_termi2; 
+  d_true_Q_err2[k] = d_true_Q_err2[k] + d_final_wavevector_err2[j] * d_termf2;
+  d_true_Q_err2[k] = d_true_Q_err2[k] + d_polar_angle_err2 * d_polar2;
+  d_true_Q_err2[k] = d_true_Q_err2[k] * d_front ;
+
   return;
 }
 
@@ -773,16 +797,18 @@ void i_function_init_scatt_wavevector_to_scalar_Q(vector<int> & i_initial_waveve
 						  int k)
 {
   int i_a;
-  int i_pang;
+  int i_pang, i_sang;
   int i_ki2, i_kf2, i_akikf;
   int i_termi, i_termi2;
   int i_termf, i_termf2;
   int i_front;
+  int i_polar, i_polar2;
 
   i_ki2 = i_initial_wavevector[i] * i_initial_wavevector[i];
 
   i_a = static_cast<int>(2 * std::cos(static_cast<double>(i_polar_angle)));
   i_pang = static_cast<int>(std::cos(static_cast<double>(i_polar_angle)));
+  i_sang = static_cast<int>(std::sin(static_cast<double>(i_polar_angle)));
 
   i_kf2 = i_final_wavevector[j] * i_final_wavevector[j];
   i_akikf = i_a * i_initial_wavevector[i] * i_final_wavevector[j];
@@ -791,13 +817,19 @@ void i_function_init_scatt_wavevector_to_scalar_Q(vector<int> & i_initial_waveve
   
   i_termi = i_initial_wavevector[i] - (i_final_wavevector[j] * i_pang);
   i_termi2 = i_termi * i_termi;
+
   i_termf = i_final_wavevector[j] - (i_initial_wavevector[i] * i_pang);
   i_termf2 = i_termf * i_termf;
-  i_front = static_cast<int>(4 / (i_true_Q[k] * i_true_Q[k]));
+
+  i_polar = i_final_wavevector[j] * i_initial_wavevector[i] * i_sang;
+  i_polar2 = i_polar * i_polar;
+
+  i_front = static_cast<int>(1 / (i_true_Q[k] * i_true_Q[k]));
   
-  i_true_Q_err2[k] = i_front * (( i_initial_wavevector_err2[i] * i_termi2) +
-				( i_final_wavevector_err2[j] * i_termf2));
-  
+  i_true_Q_err2[k] = i_initial_wavevector_err2[i] * i_termi2 + i_final_wavevector_err2[j] * i_termf2;
+  i_true_Q_err2[k] = i_true_Q_err2[k] + i_polar_angle_err2 * i_polar2;
+  i_true_Q_err2[k] = i_true_Q_err2[k] * i_front ;
+
   return;
 }
 
@@ -815,16 +847,18 @@ void u_function_init_scatt_wavevector_to_scalar_Q(vector<unsigned int> & u_initi
 						  int k)
 {
   unsigned int u_a;
-  unsigned int u_pang;
+  unsigned int u_pang, u_sang;
   unsigned int u_ki2, u_kf2, u_akikf;
   unsigned int u_termi, u_termi2;
   unsigned int u_termf, u_termf2;
   unsigned int u_front;
+  unsigned int u_polar, u_polar2;
 
   u_ki2 = u_initial_wavevector[i] * u_initial_wavevector[i];
 
   u_a = static_cast<unsigned int>(2 * std::cos(static_cast<double>(u_polar_angle)));
   u_pang = static_cast<unsigned int>(std::cos(static_cast<double>(u_polar_angle)));
+  u_sang = static_cast<unsigned int>(std::sin(static_cast<double>(u_polar_angle)));
 
   u_kf2 = u_final_wavevector[j] * u_final_wavevector[j];
   u_akikf = u_a * u_initial_wavevector[i] * u_final_wavevector[j];
@@ -833,12 +867,19 @@ void u_function_init_scatt_wavevector_to_scalar_Q(vector<unsigned int> & u_initi
   
   u_termi = u_initial_wavevector[i] - (u_final_wavevector[j] * u_pang);
   u_termi2 = u_termi * u_termi;
+
   u_termf = u_final_wavevector[j] - (u_initial_wavevector[i] * u_pang);
   u_termf2 = u_termf * u_termf;
-  u_front = 4 / (u_true_Q[k] * u_true_Q[k]);
+
+  u_polar = u_final_wavevector[j] * u_initial_wavevector[i] * u_sang;
+  u_polar2 = u_polar * u_polar;
+
+  u_front = 1 / (u_true_Q[k] * u_true_Q[k]);
   
-  u_true_Q_err2[k] = u_front * (( u_initial_wavevector_err2[i] * u_termi2) +
-				( u_final_wavevector_err2[j] * u_termf2));
+  u_true_Q_err2[k] = u_initial_wavevector_err2[i] * u_termi2 + u_final_wavevector_err2[j] * u_termf2;
+  u_true_Q_err2[k] = u_true_Q_err2[k] + u_polar_angle_err2 * u_polar2;
+  u_true_Q_err2[k] = u_true_Q_err2[k] * u_front ;
+
   
   return;
 }
