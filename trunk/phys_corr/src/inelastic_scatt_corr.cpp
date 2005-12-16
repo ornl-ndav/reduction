9ae7fb17167c9fdfa6b033f693bf912ad7afec25
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2005-12-16 21:11:29 +0000

    Actually remove int/unsigned explicit instantiations.

--- trunk/phys_corr/src/inelastic_scatt_corr.cpp
+++ trunk/phys_corr/src/inelastic_scatt_corr.cpp
@@ -35,32 +35,4 @@ namespace PhysCorr
 			       std::vector<double> & output,
 			       std::vector<double> & output_err2,
 			       void *temp);
-
-  /**
-   * This is the integer declaration of the function defined in 3.40.
-   *
-   * \see inelastic_scatt_corr()
-   */
-  template std::string
-  inelastic_scatt_corr<int>(std::vector<int> const & axis_in,
-			    std::vector<int> const & input,
-			    std::vector<int> const & input_err2,
-			    std::vector<int> & output,
-			    std::vector<int> & output_err2,
-			    void *temp);
-
-  /**
-   * This is the unsigned integer declaration of the function defined
-   * in 3.40.
-   *
-   * \see inelastic_scatt_corr()
-   */
-  template std::string
-  inelastic_scatt_corr<unsigned int>(
-                                  std::vector<unsigned int> const & axis_in,
-                                  std::vector<unsigned int> const & input,
-                                  std::vector<unsigned int> const & input_err2,
-                                  std::vector<unsigned int> & output,
-                                  std::vector<unsigned int> & output_err2,
-                                  void *temp);
 } // PhysCorr
