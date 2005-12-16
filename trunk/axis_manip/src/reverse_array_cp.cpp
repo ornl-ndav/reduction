e6c0d7946ffc01352147a99fcf8781e119fa06c1
pf9 <pf9@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2005-12-16 21:51:22 +0000

    Split definition and implementation of reverse_array_cp

--- trunk/axis_manip/src/reverse_array_cp.cpp
+++ trunk/axis_manip/src/reverse_array_cp.cpp
@@ -3,27 +3,10 @@
  *
  * \file axis_manip/src/reverse_array_cp.cpp
  */
-#include "reverse_array.hpp"
-#include <algorithm>
-#include <string>
-#include <vector>
+#include "reverse_array_cpp.hpp"
 
 namespace AxisManip
 {
-  // 3.11
-  template <typename NumT>
-  std::string 
-  reverse_array_cp(std::vector<NumT> const & input,
-		std::vector<NumT> & output,
-		void *temp=NULL)
-  {
-    std::string retstr("");
-
-    reverse_copy(input.begin(), input.end(), output.begin());
-    
-    return retstr;
-  }
-
   // explicit instantiations
 
   /**
