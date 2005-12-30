1da54a3e463e8d95b7dbd670019da418afe290be
pf9 <petersonpf@ornl.gov>
2005-12-30 22:05:05 +0000

    Added stubs for overloaded operators. Stubs throw exceptions

--- axis_manip/src/energy_transfer.cpp
+++ axis_manip/src/energy_transfer.cpp
@@ -24,6 +24,48 @@ namespace AxisManip
 			 void *temp);
   
   /**
+   * This is the float declaration of the function defined in 3.30.
+   *
+   * \see energy_transfer()
+   */
+  template std::string
+  energy_transfer<float>(Nessi::Vector<float> const & initial_energy,
+			 Nessi::Vector<float> const & initial_energy_err2,
+			 const float final_energy,
+			 const float final_energy_err2,
+			 Nessi::Vector<float> & energy_transfer,
+			 Nessi::Vector<float> & energy_transfer_err2,
+			 void *temp);
+  
+  /**
+   * This is the float declaration of the function defined in 3.30.
+   *
+   * \see energy_transfer()
+   */
+  template std::string
+  energy_transfer<float>(const float initial_energy,
+			 const float initial_energy_err2,
+			 Nessi::Vector<float> const & final_energy,
+			 Nessi::Vector<float> const & final_energy_err2,
+			 Nessi::Vector<float> & energy_transfer,
+			 Nessi::Vector<float> & energy_transfer_err2,
+			 void *temp);
+  
+  /**
+   * This is the float declaration of the function defined in 3.30.
+   *
+   * \see energy_transfer()
+   */
+  template std::string
+  energy_transfer<float>(const float initial_energy,
+			 const float initial_energy_err2,
+			 const float final_energy,
+			 float final_energy_err2,
+			 float & energy_transfer,
+			 float & energy_transfer_err2,
+			 void *temp);
+  
+  /**
    * This is the double precision float declaration of the function
    * defined in 3.30.
    *
@@ -36,5 +78,50 @@ namespace AxisManip
 			  Nessi::Vector<double> const & final_energy_err2,
 			  Nessi::Vector<double> & energy_transfer,
 			  Nessi::Vector<double> & energy_transfer_err2,
+			  void *temp);
+
+  /**
+   * This is the double precision float declaration of the function
+   * defined in 3.30.
+   *
+   * \see energy_transfer()
+   */
+  template std::string
+  energy_transfer<double>(Nessi::Vector<double> const & initial_energy,
+			  Nessi::Vector<double> const & initial_energy_err2,
+			  const double final_energy,
+			  const double final_energy_err2,
+			  Nessi::Vector<double> & energy_transfer,
+			  Nessi::Vector<double> & energy_transfer_err2,
+			  void *temp);
+
+  /**
+   * This is the double precision float declaration of the function
+   * defined in 3.30.
+   *
+   * \see energy_transfer()
+   */
+  template std::string
+  energy_transfer<double>(const double initial_energy,
+			  const double initial_energy_err2,
+			  Nessi::Vector<double> const & final_energy,
+			  Nessi::Vector<double> const & final_energy_err2,
+			  Nessi::Vector<double> & energy_transfer,
+			  Nessi::Vector<double> & energy_transfer_err2,
+			  void *temp);
+
+  /**
+   * This is the double precision float declaration of the function
+   * defined in 3.30.
+   *
+   * \see energy_transfer()
+   */
+  template std::string
+  energy_transfer<double>(const double initial_energy,
+			  const double initial_energy_err2,
+			  const double final_energy,
+			  const double final_energy_err2,
+			  double & energy_transfer,
+			  double & energy_transfer_err2,
 			  void *temp);
 } // AxisManip
--- axis_manip/src/init_scatt_wavevector_to_Q.cpp
+++ axis_manip/src/init_scatt_wavevector_to_Q.cpp
@@ -31,6 +31,75 @@ namespace AxisManip
                           Nessi::Vector<float> & Qz,
                           Nessi::Vector<float> & Qz_err2,
                           void *temp=NULL);
+
+  /**
+   * This is the float declaration of the function defined in 3.32.
+   *
+   * \see init_scatt_wavevector_to_Q()
+   */
+  template std::string
+  init_scatt_wavevector_to_Q<float>(
+                          const float initial_wavevector,
+                          const float initial_wavevector_err2,
+                          Nessi::Vector<float> const & final_wavevector,
+                          Nessi::Vector<float> const & final_wavevector_err2,
+                          const float azimuthal_angle,
+                          const float azimuthal_angle_err2,
+                          const float polar_angle,
+                          const float polar_angle_err2,
+                          Nessi::Vector<float> & Qx,
+                          Nessi::Vector<float> & Qx_err2,
+                          Nessi::Vector<float> & Qy,
+                          Nessi::Vector<float> & Qy_err2,
+                          Nessi::Vector<float> & Qz,
+                          Nessi::Vector<float> & Qz_err2,
+                          void *temp=NULL);
+
+  /**
+   * This is the float declaration of the function defined in 3.32.
+   *
+   * \see init_scatt_wavevector_to_Q()
+   */
+  template std::string
+  init_scatt_wavevector_to_Q<float>(
+                          Nessi::Vector<float> const & initial_wavevector,
+                          Nessi::Vector<float> const & initial_wavevector_err2,
+                          const float final_wavevector,
+                          const float final_wavevector_err2,
+                          const float azimuthal_angle,
+                          const float azimuthal_angle_err2,
+                          const float polar_angle,
+                          const float polar_angle_err2,
+                          Nessi::Vector<float> & Qx,
+                          Nessi::Vector<float> & Qx_err2,
+                          Nessi::Vector<float> & Qy,
+                          Nessi::Vector<float> & Qy_err2,
+                          Nessi::Vector<float> & Qz,
+                          Nessi::Vector<float> & Qz_err2,
+                          void *temp=NULL);
+
+  /**
+   * This is the float declaration of the function defined in 3.32.
+   *
+   * \see init_scatt_wavevector_to_Q()
+   */
+  template std::string
+  init_scatt_wavevector_to_Q<float>(
+                          const float initial_wavevector,
+                          const float initial_wavevector_err2,
+                          const float final_wavevector,
+                          const float final_wavevector_err2,
+                          const float azimuthal_angle,
+                          const float azimuthal_angle_err2,
+                          const float polar_angle,
+                          const float polar_angle_err2,
+                          float & Qx,
+                          float & Qx_err2,
+                          float & Qy,
+                          float & Qy_err2,
+                          float & Qz,
+                          float & Qz_err2,
+                          void *temp=NULL);
   
   /**
    * This is the double precision float declaration of the function
@@ -54,5 +123,77 @@ namespace AxisManip
                          Nessi::Vector<double> & Qy_err2,
                          Nessi::Vector<double> & Qz,
                          Nessi::Vector<double> & Qz_err2,
+                         void *temp=NULL);
+
+  /**
+   * This is the double precision float declaration of the function
+   * defined in 3.32.
+   *
+   * \see init_scatt_wavevector_to_Q()
+   */
+  template std::string
+  init_scatt_wavevector_to_Q<double>(
+                         const double initial_wavevector,
+                         const double initial_wavevector_err2,
+                         Nessi::Vector<double> const & final_wavevector,
+                         Nessi::Vector<double> const & final_wavevector_err2,
+                         const double azimuthal_angle,
+                         const double azimuthal_angle_err2,
+                         const double polar_angle,
+                         const double polar_angle_err2,
+                         Nessi::Vector<double> & Qx,
+                         Nessi::Vector<double> & Qx_err2,
+                         Nessi::Vector<double> & Qy,
+                         Nessi::Vector<double> & Qy_err2,
+                         Nessi::Vector<double> & Qz,
+                         Nessi::Vector<double> & Qz_err2,
+                         void *temp=NULL);
+
+  /**
+   * This is the double precision float declaration of the function
+   * defined in 3.32.
+   *
+   * \see init_scatt_wavevector_to_Q()
+   */
+  template std::string
+  init_scatt_wavevector_to_Q<double>(
+                         Nessi::Vector<double> const & initial_wavevector,
+                         Nessi::Vector<double> const & initial_wavevector_err2,
+                         const double final_wavevector,
+                         const double final_wavevector_err2,
+                         const double azimuthal_angle,
+                         const double azimuthal_angle_err2,
+                         const double polar_angle,
+                         const double polar_angle_err2,
+                         Nessi::Vector<double> & Qx,
+                         Nessi::Vector<double> & Qx_err2,
+                         Nessi::Vector<double> & Qy,
+                         Nessi::Vector<double> & Qy_err2,
+                         Nessi::Vector<double> & Qz,
+                         Nessi::Vector<double> & Qz_err2,
+                         void *temp=NULL);
+
+  /**
+   * This is the double precision float declaration of the function
+   * defined in 3.32.
+   *
+   * \see init_scatt_wavevector_to_Q()
+   */
+  template std::string
+  init_scatt_wavevector_to_Q<double>(
+                         const double initial_wavevector,
+                         const double initial_wavevector_err2,
+                         const double final_wavevector,
+                         const double final_wavevector_err2,
+                         const double azimuthal_angle,
+                         const double azimuthal_angle_err2,
+                         const double polar_angle,
+                         const double polar_angle_err2,
+                         double & Qx,
+                         double & Qx_err2,
+                         double & Qy,
+                         double & Qy_err2,
+                         double & Qz,
+                         double & Qz_err2,
                          void *temp=NULL);
 } // AxisManip
