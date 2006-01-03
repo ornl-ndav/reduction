e8753e01128e34ced469abf26fef2783e46035fa
pf9 <petersonpf@ornl.gov>
2006-01-03 14:13:41 +0000

    New set of functions that replace fd_comparison and iu_comparison

---
+++ utils/src/vector_comparison.hpp
@@ -0,0 +1,33 @@
+/**
+ * $Id: $
+ *
+ * \file utils/src/vector_comparison.hpp
+ */
+
+#ifndef _VECTOR_COMPARISON_HPP
+#define _VECTOR_COMPARISON_HPP
+
+#include "num_comparison.hpp"
+
+namespace Utils
+{
+  template <typename NumT>
+  void
+  vector_comparison (Nessi::Vector<NumT> const & value,
+                     Nessi::Vector<NumT> const & true_value,
+                     int & error,
+                     const int add_error,
+                     const int n_max)
+  {
+    for (int i = 0 ; i < n_max ; i++)
+      {
+	if (value[i] != true_value[i])
+	  {
+	    error += add_error;
+	    break;
+	  }
+      }
+    return;
+  }
+}
+#endif
