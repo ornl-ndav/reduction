45819535b6bc143b4cd839e0bba6c2f2225e9926
2zr <mareuternh@gmail.com>
2006-01-04 15:59:47 +0000

    Added documentation and fixed guard name.

--- nessi/nessi.hpp
+++ nessi/nessi.hpp
@@ -1,29 +1,46 @@
 /**
  * $Id$
  *
- * \file inc\nessi.hpp
+ * \file nessi/nessi.hpp
  */
 
-#ifndef _NESSI_H
-#define _NESSI_H 1
+#ifndef _NESSI_HPP
+#define _NESSI_HPP 1
 
 #include <vector>
 
 /**
  * This file contains the abstraction of std::vector to Nessi::Vector which 
  * eliminates explicit calls to std::vector. This allows easy swapping of 
- * std::vector for another container.
+ * std::vector for another container. 
  */
-namespace Nessi {
-
+namespace Nessi 
+{
+  /**
+   * \brief Provides wrapper for std::vector
+   *
+   * 
+   */
 template <typename T>
 class Vector : public std::vector<T>
 {
 public:
+  /**
+   * \brief Constructor for empty Vector
+   *
+   * This provides a constructor method for an empty Vector
+   */
   Vector() : std::vector<T>() { }
+  /**
+   * \brief Constructor for Vector of size i
+   *
+   * This provides a constructor method for Vector of size i
+   *
+   * \param i is the size of the desired Vector
+   */
   Vector(int i) : std::vector<T>(i) { }
 };
 
-}
+} // Nessi
 
-#endif // _NESSI_H
+#endif // _NESSI_HPP
