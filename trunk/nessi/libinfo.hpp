89393571c178668239c8115fae3272aa17f9395a
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-05 15:08:26 +0000

    Providing way to add information to the main documentation page.

---
+++ trunk/nessi/libinfo.hpp
@@ -0,0 +1,13 @@
+/**
+ * $Id$
+ *
+ * \file nessi/libinfo.hpp
+ */
+/** 
+ * The purpose of this file is to add information to the main index page for 
+ * the SNS Data Reduction Library documentation.
+ *
+ * \mainpage SNS Data Reduction Library 1.0.0alpha
+ *
+ * \section Definitions
+ */
