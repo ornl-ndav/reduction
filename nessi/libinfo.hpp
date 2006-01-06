94a599749088ab007de26d5b070874b2b5d10ef3
pf9 <petersonpf@ornl.gov>
2006-01-06 18:57:20 +0000

    Fixed formatting problems

--- nessi/libinfo.hpp
+++ nessi/libinfo.hpp
@@ -9,7 +9,7 @@
  *
  * \mainpage SNS Data Reduction Library 1.0.0alpha
  *
- * \section Rules of the Library
+ * \section rules Rules of the Library
  *
  * To provide the greatest inter-language and inter-facility
  * operability, the data reduction library will be implemented as C++
@@ -18,14 +18,14 @@
  * the library is well-formed, does not affect outside systems and
  * does not maintain state. The rules are:
  *
- * # There will be no global/package variables.
- * # There will be no side effects.
- * # They will be thread safe.
- * # They will neither allocate nor de-allocate any persistent
+ * -# There will be no global/package variables.
+ * -# There will be no side effects.
+ * -# They will be thread safe.
+ * -# They will neither allocate nor de-allocate any persistent
  * resources. This allows for allocating (and de-allocating) temporary
  * memory for calculations.
- * # There will be at least one unit test for every function.
- * # The functions will need to be passed the arguments, space for the
+ * -# There will be at least one unit test for every function.
+ * -# The functions will need to be passed the arguments, space for the
  * results, and space for any temporary storage needed. To notify the
  * caller of how much memory is needed for temporary space, any
  * function that uses temporary space must have a related utility
@@ -33,9 +33,9 @@
  * storage needed. If the user passes in null pointers for the
  * temporary storage the function is intended to allocate and
  * de-allocate what is needed.
- * # The functions will return an integer status report with a helper
+ * -# The functions will return an integer status report with a helper
  * function that creates strings according to that value.
  *
- * \section Function Naming Conventions
+ * \section naming_convention Function Naming Conventions
  *
  */
