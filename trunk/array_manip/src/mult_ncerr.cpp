1be11a0d8c9d955a6174b067bab021bb9619328d
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-06 02:19:30 +0000

    Fixed const location on input vectors.

--- trunk/array_manip/src/mult_ncerr.cpp
+++ trunk/array_manip/src/mult_ncerr.cpp
@@ -15,8 +15,8 @@ namespace ArrayManip
    * \see mult_scalar_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<float>(Nessi::Vector<float> const & array_in, 
-                    Nessi::Vector<float> const & array_in_err2, 
+  mult_ncerr<float>(const Nessi::Vector<float> & array_in, 
+                    const Nessi::Vector<float> & array_in_err2, 
                     const float scalar,
                     const float scalar_err2,
                     Nessi::Vector<float> & array_out,
@@ -30,8 +30,8 @@ namespace ArrayManip
    * \see mult_scalar_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<double>(Nessi::Vector<double> const & array_in, 
-                     Nessi::Vector<double> const & array_in_err2, 
+  mult_ncerr<double>(const Nessi::Vector<double> & array_in, 
+                     const Nessi::Vector<double> & array_in_err2, 
                      const double scalar,
                      const double scalar_err2,
                      Nessi::Vector<double> & array_out,
@@ -44,8 +44,8 @@ namespace ArrayManip
    * \see mult_scalar_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<int>(Nessi::Vector<int> const & array_in, 
-                  Nessi::Vector<int> const & array_in_err2, 
+  mult_ncerr<int>(const Nessi::Vector<int> & array_in, 
+                  const Nessi::Vector<int> & array_in_err2, 
                   const int scalar,
                   const int scalar_err2,
                   Nessi::Vector<int> & array_out,
@@ -59,8 +59,8 @@ namespace ArrayManip
    * \see mult_scalar_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<unsigned int>(Nessi::Vector<unsigned int> const & array_in, 
-                           Nessi::Vector<unsigned int> const & array_in_err2, 
+  mult_ncerr<unsigned int>(const Nessi::Vector<unsigned int> & array_in, 
+                           const Nessi::Vector<unsigned int> & array_in_err2, 
                            const unsigned int scalar, 
                            const unsigned int scalar_err2,
                            Nessi::Vector<unsigned int> & array_out,
@@ -73,10 +73,10 @@ namespace ArrayManip
    * \see mult_vec_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<float>(Nessi::Vector<float> const & input1, 
-                    Nessi::Vector<float> const & input1_err2, 
-                    Nessi::Vector<float> const & input2, 
-                    Nessi::Vector<float> const & input2_err2, 
+  mult_ncerr<float>(const Nessi::Vector<float> & input1, 
+                    const Nessi::Vector<float> & input1_err2, 
+                    const Nessi::Vector<float> & input2, 
+                    const Nessi::Vector<float> & input2_err2, 
                     Nessi::Vector<float> & output,
                     Nessi::Vector<float> & output_err2,
                     void *temp);
@@ -88,10 +88,10 @@ namespace ArrayManip
    * \see mult_vec_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<double>(Nessi::Vector<double> const & input1, 
-                     Nessi::Vector<double> const & input1_err2, 
-                     Nessi::Vector<double> const & input2, 
-                     Nessi::Vector<double> const & input2_err2, 
+  mult_ncerr<double>(const Nessi::Vector<double> & input1, 
+                     const Nessi::Vector<double> & input1_err2, 
+                     const Nessi::Vector<double> & input2, 
+                     const Nessi::Vector<double> & input2_err2, 
                      Nessi::Vector<double> & output,
                      Nessi::Vector<double> & output_err2,
                      void *temp);
@@ -102,10 +102,10 @@ namespace ArrayManip
    * \see mult_vec_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<int>(Nessi::Vector<int> const & input1, 
-                  Nessi::Vector<int> const & input1_err2, 
-                  Nessi::Vector<int> const & input2, 
-                  Nessi::Vector<int> const & input2_err2, 
+  mult_ncerr<int>(const Nessi::Vector<int> & input1, 
+                  const Nessi::Vector<int> & input1_err2, 
+                  const Nessi::Vector<int> & input2, 
+                  const Nessi::Vector<int> & input2_err2, 
                   Nessi::Vector<int> & output,
                   Nessi::Vector<int> & output_err2,
                   void *temp);
@@ -117,10 +117,10 @@ namespace ArrayManip
    * \see mult_vec_vec_ncerr()
    */
   template std::string 
-  mult_ncerr<unsigned int>(Nessi::Vector<unsigned int> const & input1, 
-                           Nessi::Vector<unsigned int> const & input1_err2, 
-                           Nessi::Vector<unsigned int> const & input2, 
-                           Nessi::Vector<unsigned int> const & input2_err2, 
+  mult_ncerr<unsigned int>(const Nessi::Vector<unsigned int> & input1, 
+                           const Nessi::Vector<unsigned int> & input1_err2, 
+                           const Nessi::Vector<unsigned int> & input2, 
+                           const Nessi::Vector<unsigned int> & input2_err2, 
                            Nessi::Vector<unsigned int> & output,
                            Nessi::Vector<unsigned int> & output_err2,
                            void *temp);
--- trunk/array_manip/src/mult_ncerr.hpp
+++ trunk/array_manip/src/mult_ncerr.hpp
@@ -15,8 +15,8 @@ namespace ArrayManip
   // 3.3
   template <typename NumT>
   std::string 
-  mult_ncerr(Nessi::Vector<NumT> const & array_in, 
-             Nessi::Vector<NumT> const & array_in_err2, 
+  mult_ncerr(const Nessi::Vector<NumT> & array_in, 
+             const Nessi::Vector<NumT> & array_in_err2, 
              const NumT scalar,
              const NumT scalar_err2,
              Nessi::Vector<NumT> & array_out,
@@ -38,10 +38,10 @@ namespace ArrayManip
   // 3.8
   template <typename NumT>
   std::string 
-  mult_ncerr(Nessi::Vector<NumT> const & input1, 
-             Nessi::Vector<NumT> const & input1_err2, 
-             Nessi::Vector<NumT> const & input2, 
-             Nessi::Vector<NumT> const & input2_err2, 
+  mult_ncerr(const Nessi::Vector<NumT> & input1, 
+             const Nessi::Vector<NumT> & input1_err2, 
+             const Nessi::Vector<NumT> & input2, 
+             const Nessi::Vector<NumT> & input2_err2, 
              Nessi::Vector<NumT> & output,
              Nessi::Vector<NumT> & output_err2,
              void *temp=NULL)
