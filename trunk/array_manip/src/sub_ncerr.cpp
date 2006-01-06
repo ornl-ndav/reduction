db4f7c3add76915c0abd83196ff59ad6cfb289ab
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-06 02:14:21 +0000

    Fixed const correctness for scalars.

--- trunk/array_manip/src/sub_ncerr.cpp
+++ trunk/array_manip/src/sub_ncerr.cpp
@@ -17,8 +17,8 @@ namespace ArrayManip
   template std::string 
   sub_ncerr<float>(const Nessi::Vector<float> & array_in, 
                    const Nessi::Vector<float> & array_in_err2, 
-                   float scalar,
-                   float scalar_err2,
+                   const float scalar,
+                   const float scalar_err2,
                    Nessi::Vector<float> & array_out,
                    Nessi::Vector<float> & array_out_err2,
                    void *temp);
@@ -32,8 +32,8 @@ namespace ArrayManip
   template std::string 
   sub_ncerr<double>(const Nessi::Vector<double> & array_in, 
                     const Nessi::Vector<double> & array_in_err2, 
-                    double scalar,
-                    double scalar_err2,
+                    const double scalar,
+                    const double scalar_err2,
                     Nessi::Vector<double> & array_out,
                     Nessi::Vector<double> & array_out_err2,
                     void *temp);
@@ -46,8 +46,8 @@ namespace ArrayManip
   template std::string 
   sub_ncerr<int>(const Nessi::Vector<int> & array_in, 
                  const Nessi::Vector<int> & array_in_err2, 
-                 int scalar,
-                 int scalar_err2,
+                 const int scalar,
+                 const int scalar_err2,
                  Nessi::Vector<int> & array_out,
                  Nessi::Vector<int> & array_out_err2,
                  void *temp);
@@ -61,8 +61,8 @@ namespace ArrayManip
   template std::string 
   sub_ncerr<unsigned int>(const Nessi::Vector<unsigned int> & array_in, 
                           const Nessi::Vector<unsigned int> & array_in_err2, 
-                          unsigned int scalar, 
-                          unsigned int scalar_err2,
+                          const unsigned int scalar, 
+                          const unsigned int scalar_err2,
                           Nessi::Vector<unsigned int> & array_out,
                           Nessi::Vector<unsigned int> & array_out_err2,
                           void *temp);
