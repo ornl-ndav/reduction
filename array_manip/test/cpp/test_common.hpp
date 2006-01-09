6fc724ad888bf2c9d23848eebb3c31e77d15c390
Jean Bilheux <bilheuxjm@ornl.gov>
2006-01-09 16:33:30 +0000

    Removed general call to std template

--- array_manip/test/cpp/test_common.hpp
+++ array_manip/test/cpp/test_common.hpp
@@ -4,55 +4,53 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-const string EMPTY("");
-const string ERROR("error ");
-const string SV("s,v");
-const string VS("v,s");
-const string VV("v,v");
+const std::string EMPTY("");
+const std::string ERROR("error ");
+const std::string SV("s,v");
+const std::string VS("v,s");
+const std::string VV("v,v");
 
 template <typename NumT>
-string type_string(Nessi::Vector<NumT> & vec)
+std::string type_string(Nessi::Vector<NumT> & vec)
 {
-  return string(EMPTY);
+  return std::string(EMPTY);
 }
 
-string type_string(Nessi::Vector<double> & vec)
+std::string type_string(Nessi::Vector<double> & vec)
 {
-  return string("double");
+  return std::string("double");
 }
 
-string type_string(Nessi::Vector<float> & vec)
+std::string type_string(Nessi::Vector<float> & vec)
 {
-  return string("float");
+  return std::string("float");
 }
 
-string type_string(Nessi::Vector<int> & vec)
+std::string type_string(Nessi::Vector<int> & vec)
 {
-  return string("int");
+  return std::string("int");
 }
 
-string type_string(Nessi::Vector<unsigned> & vec)
+std::string type_string(Nessi::Vector<unsigned> & vec)
 {
-  return string("unsigned");
+  return std::string("unsigned");
 }
 
 // returns true if nothing is wrong
 template <typename NumT>
 bool test_okay(Nessi::Vector<NumT> & output,
                Nessi::Vector<NumT> & true_output,
-               string array_type,
-               string data_type=EMPTY){
+               std::string array_type,
+               std::string data_type=EMPTY){
 
   int error=0; // dummy variable for usin in testing code
 
   Utils::vector_comparison(output,true_output,error,10);
   if(error>0)
     {
-      cout << "(" << type_string(output) << " " << array_type
+      std::cout << "(" << type_string(output) << " " << array_type
            << ") FAILED....Output " << data_type
-           << "different from vector expected" << endl;
+           << "different from vector expected" << std::endl;
       return false;
     }
   return true;
