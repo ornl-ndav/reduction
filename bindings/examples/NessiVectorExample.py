4d803a1f9206e12e718a4c6d2c7017f5ef2ee198
Jean Bilheux <bilheuxjm@ornl.gov>
2006-01-09 19:41:44 +0000

    Added code that used the new add(a,b) and add(a,ae2,b,be2) functions

--- bindings/examples/NessiVectorExample.py
+++ bindings/examples/NessiVectorExample.py
@@ -6,10 +6,13 @@
 #
 ##############################################
 
-import NessiVector
-from NessiVector import *
+from NessiVector import NessiVector
+#from array_manip_bind import *
+from array_manip_bind import add_ncerr_f
 
 import sys
+import array_manip
+from array_manip import add
 
 def print_vect(object):
     for i in range(len(object)):
@@ -17,9 +20,9 @@ def print_vect(object):
 
     print
 
-
 print "Checking Array Manipulation Package for float"
 
+#Initialization of vectors a and b
 a = NessiVector()
 a.append(1.5)
 a.append(3.5)
@@ -43,17 +46,28 @@ be2.append(0.65)
 c = NessiVector(len(a))
 ce2 = NessiVector(len(ae2))
 
-print "Array A: "
-print_vect(a)
-print "Array A error2: "
-print_vect(ae2)
-print "Array B: "
-print_vect(b)
-print "Array B error2: "
-print_vect(be2)
+c=add(a,b)
+print ">>>c=add(a,b)"
+print "a\t\tb\t\tc=a+b\t\n",
+print "**********************************************"
+print a[0], "\t\t", b[0],"\t\t",c[0]
+print a[1], "\t\t", b[1],"\t\t",c[1]
+print a[2], "\t\t", b[2],"\t\t",c[2],
+print "\n"
 
-add_vec_vec_ncerr_f(a.array,ae2.array,b.array,be2.array,c.array,ce2.array)
-print "Added NessiVector: "
-print_vect(c)
-print "Added NessiVector error2: "
-print_vect(ce2)
+c,ce2 = add(a,ae2,b,be2)
+print ">>>c,ce2=add(a,ae2,b,be2)"
+print "a\t\tb\t\tc=a+b\t\n",
+print "**********************************************"
+print a[0], "\t\t", b[0],"\t\t",c[0]
+print a[1], "\t\t", b[1],"\t\t",c[1]
+print a[2], "\t\t", b[2],"\t\t",c[2],
+print "\n"
+print "ae2\t\tbe2\t\tce2=ae2+be2\n",
+print "**********************************************"
+print "%2.2f" %ae2[0], "\t\t", "%2.2f" %be2[0],"\t\t" "%2.2f" %ce2[0]
+print "%2.2f" %ae2[1], "\t\t", "%2.2f" %be2[1],"\t\t","%2.2f" %ce2[1]
+print "%2.2f" %ae2[2], "\t\t", "%2.2f" %be2[2],"\t\t","%2.2f" %ce2[2],
+print "\n"
+
+
