0cc9dc764c24c2bb70b3c3c4173b8598df999970
j35 <j35@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-18 22:51:56 +0000

    turn most of the ints into ints with dots

--- trunk/array_manip/test/cpp/div_ncerr_test.cpp
+++ trunk/array_manip/test/cpp/div_ncerr_test.cpp
@@ -1,7 +1,7 @@
 // $Id$
 
-#include "test_common.hpp"
 #include "arith.hpp"
+#include "test_common.hpp"
 
 using namespace std;
 
@@ -15,10 +15,10 @@ void initialize_inputs(Nessi::Vector<NumT> & input1,
 {
   for( int i=0 ; i<NUM_VAL ; i++ )
     {
-      input1.push_back(static_cast<NumT>(10-i)); // 10,9,8,7,6
-      input2.push_back(static_cast<NumT>(2+i));  //  2,3,4,5,6
-      input1_err2.push_back(static_cast<NumT>(1));
-      input2_err2.push_back(static_cast<NumT>(1));
+      input1.push_back(static_cast<NumT>(10.-i)); // 10,9,8,7,6
+      input2.push_back(static_cast<NumT>(2.+i));  //  2,3,4,5,6
+      input1_err2.push_back(static_cast<NumT>(1.));
+      input2_err2.push_back(static_cast<NumT>(1.));
     }
 }
 
@@ -32,37 +32,37 @@ void initialize_true_outputs(Nessi::Vector<NumT> & output_vs,
                              Nessi::Vector<NumT> & output_vv_err2)
 {
   // initialize the correct outputs for vector vector case
-  output_vv.push_back(static_cast<NumT>(5)); // =10/2
+  output_vv.push_back(static_cast<NumT>(5.)); // =10/2
   output_vv_err2.push_back(static_cast<NumT>(6.5));
-  output_vv.push_back(static_cast<NumT>(3)); // =9/3
+  output_vv.push_back(static_cast<NumT>(3.)); // =9/3
   output_vv_err2.push_back(static_cast<NumT>(1.11111111111111111111111111111));
-  output_vv.push_back(static_cast<NumT>(2)); // =8/4
+  output_vv.push_back(static_cast<NumT>(2.)); // =8/4
   output_vv_err2.push_back(static_cast<NumT>(0.3125));
   output_vv.push_back(static_cast<NumT>(1.4)); // =7/5
   output_vv_err2.push_back(static_cast<NumT>(0.1184));
-  output_vv.push_back(static_cast<NumT>(1)); // =6/6
+  output_vv.push_back(static_cast<NumT>(1.)); // =6/6
   output_vv_err2.push_back(static_cast<NumT>(0.05555555555555555555555555556));
 
   // initialize the correct outputs for vector scalar case
-  output_vs.push_back(static_cast<NumT>(5)); // =10/2
+  output_vs.push_back(static_cast<NumT>(5.)); // =10/2
   output_vs_err2.push_back(static_cast<NumT>(6.5));
   output_vs.push_back(static_cast<NumT>(4.5)); // =9/2
   output_vs_err2.push_back(static_cast<NumT>(5.3125));
-  output_vs.push_back(static_cast<NumT>(4)); // =8/2
+  output_vs.push_back(static_cast<NumT>(4.)); // =8/2
   output_vs_err2.push_back(static_cast<NumT>(4.25));
   output_vs.push_back(static_cast<NumT>(3.5)); // =7/2
   output_vs_err2.push_back(static_cast<NumT>(3.3125));
-  output_vs.push_back(static_cast<NumT>(3)); // =6/2
+  output_vs.push_back(static_cast<NumT>(3.)); // =6/2
   output_vs_err2.push_back(static_cast<NumT>(2.5));
 
   // initialize the correct outputs for scalar vector case
-  output_sv.push_back(static_cast<NumT>(5)); // =10/2
+  output_sv.push_back(static_cast<NumT>(5.)); // =10/2
   output_sv_err2.push_back(static_cast<NumT>(6.5));
   output_sv.push_back(static_cast<NumT>(3.33333333333333333333333333)); //=10/3
   output_sv_err2.push_back(static_cast<NumT>(1.34567901234567899));
   output_sv.push_back(static_cast<NumT>(2.5)); // =10/4
   output_sv_err2.push_back(static_cast<NumT>(0.453125));
-  output_sv.push_back(static_cast<NumT>(2)); // =10/5
+  output_sv.push_back(static_cast<NumT>(2.)); // =10/5
   output_sv_err2.push_back(static_cast<NumT>(0.2));
   output_sv.push_back(static_cast<NumT>(1.66666666666666666666666667)); //=10/6
   output_sv_err2.push_back(static_cast<NumT>(0.104938271604938266));
