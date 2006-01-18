c345171ed8e096472de45fcc5c2cfd821532f4db
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-18 14:16:54 +0000

    New directories.

---
+++ trunk/phys_const/inc/constants.hpp
@@ -0,0 +1,43 @@
+/**
+ * $Id$
+ *
+ * \file phys_const/inc/constants.hpp
+ */
+#ifndef _CONSTANTS_HPP
+#define _CONSTANTS_HPP 1
+
+/**
+ * \namespace PhysConst
+ *
+ * \brief A collection of double precision constants useful in
+ * time-of-flight neutron scattering
+ */
+namespace PhysConst
+{
+  /**
+   * The value of \f$\pi\f$ to 29 decimal digits.
+   */
+  static const double PI = 3.14159265358979323846264338328;
+
+  /**
+   * The value of \f$h/m_n\f$ where \f$h\f$ is Plank's constant and
+   * \f$m_n\f$ is the mass of the neutron. The units of this constant
+   * is \f$meters \times Angstroms / microseconds\f$
+   */
+  static const double H_OVER_MNEUT = 0.003961617; 
+
+  /**
+   * The value of \f$h^2/(2 m_n)\f$ where \f$h\f$ is Plank's constant
+   * and \f$m_n\f$ is the mass of the neutron. The units of this
+   * constant is \f$ Angstroms^2 \times meV\f$
+   */
+  static const double H2_OVER_2MNEUT = 81.83;
+
+  /**
+   * The value of Plank's constant, \f$h\f$, in units of \f$meV /
+   * THz\f$
+   */
+  static const double H = 4.1357;
+}
+
+#endif // _CONSTANTS_HPP
