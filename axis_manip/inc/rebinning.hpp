fa3401a39b295b1856cc757a26015144db2b1120
2zr <mareuternh@gmail.com>
2006-01-19 22:04:16 +0000

    Adding documentation for rebin_axis_1D function.

--- axis_manip/inc/rebinning.hpp
+++ axis_manip/inc/rebinning.hpp
@@ -24,6 +24,88 @@ namespace AxisManip
    * perform the rebinning process. The function also assumes that the data
    * is represented by a histogram model.
    *
+   * To show the effects of rebinning, an example will now be discussed. We 
+   * start with a histogram containing 3 bins, which runs from 0 to 3 on its 
+   * x-axis. So, the histogram looks like:
+   *
+   * <CENTER>
+   * <TABLE>
+   * <TR>
+   * <TH>X-Axis Value</TH>
+   * <TH>Counts</TH>
+   * <TH>\f$\sigma^2\f$</TH>
+   * </TR>
+   * <TR ALIGN="CENTER"><TD>0</TD><TD>10</TD><TD>1</TD></TR>
+   * <TR ALIGN="CENTER"><TD>1</TD><TD>20</TD><TD>4</TD></TR>
+   * <TR ALIGN="CENTER"><TD>2</TD><TD>30</TD><TD>9</TD></TR>
+   * <TR ALIGN="CENTER"><TD>3</TD><TD>  </TD><TD> </TD></TR>
+   * </TABLE>
+   * </CENTER>
+   *
+   * Notice that the final x-axis value is not associated with a count value. 
+   * This is a property of the histogram data model. Our new histogram has the 
+   * same x-axis range [0,3], but it only has two bins. The result of the 
+   * rebinned histogram is shown in the table below. 
+   * <CENTER>
+   * <TABLE>
+   * <TR>
+   * <TH>X-Axis Value</TH>
+   * <TH>Counts</TH>
+   * <TH>\f$\sigma^2\f$</TH>
+   * </TR>
+   * <TR ALIGN="CENTER"><TD>0</TD><TD>20</TD><TD>2</TD></TR>
+   * <TR ALIGN="CENTER"><TD>1.5</TD><TD>40</TD><TD>10</TD></TR>
+   * <TR ALIGN="CENTER"><TD>3</TD><TD>  </TD><TD> </TD></TR>
+   * </TABLE>
+   * </CENTER>
+   * 
+   * The new bin boundary (1.5) lies in the middle of the second bin in the 
+   * original histogram. That means that its contents will be evenly split 
+   * into the new bins. So,
+   * \f[
+   * Bin_1^{new} = Bin_1^{old} + 0.5 \times Bin_2^{old}
+   * \f]
+   * \f[
+   * Bin_2^{new} = Bin_3^{old} + 0.5 \times Bin_2^{old}
+   * \f]
+   *
+   * This can be seen in the results presented in the above table. Now, we'll 
+   * reverse the process. The original histogram is the rebinned histogram 
+   * with two bins used previously and the new rebinned histogram will have 
+   * the same number of bins (3) as the previously used original histogram. 
+   * One can see that we have the following distribution of bin contents:
+   *
+   * \f[
+   * Bin_1^{new} = \frac{2}{3} \times Bin_1^{old}
+   * \f]
+   * \f[
+   * Bin_2^{new} = \frac{1}{3} \times Bin_1^{old} + \frac{1}{3} \times 
+   * Bin_2^{old}
+   * \f]
+   * \f[
+   * Bin_3^{new} = \frac{2}{3} \times Bin_3^{old}
+   * \f]
+   *
+   * The results of this operation are shown in the following table:
+   *
+   * <CENTER>
+   * <TABLE>
+   * <TR>
+   * <TH>X-Axis Value</TH>
+   * <TH>Counts</TH>
+   * <TH>\f$\sigma^2\f$</TH>
+   * </TR>
+   * <TR ALIGN="CENTER"><TD>0</TD><TD>13.33333</TD><TD>0.88888</TD></TR>
+   * <TR ALIGN="CENTER"><TD>1</TD><TD>20</TD><TD>1.33333</TD></TR>
+   * <TR ALIGN="CENTER"><TD>2</TD><TD>26.66666</TD><TD>4.44444</TD></TR>
+   * <TR ALIGN="CENTER"><TD>3</TD><TD>  </TD><TD> </TD></TR>
+   * </TABLE>
+   * </CENTER>
+   *
+   * As one can see, these values are different from the first table shown in 
+   * this example. This is due to the loss of information when performing a 
+   * rebin on data. Therefore, rebin your data thoughtfully and carefully!
+   *
    * This function was provided by Paul Kienzle (NIST) and is based on an
    * OpenGenie routine.
    *
