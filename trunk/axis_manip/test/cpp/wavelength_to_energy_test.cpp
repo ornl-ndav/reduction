25e788b8c94dc598352038d181bc99ebc16383f6
j35 <j35@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-25 21:04:28 +0000

    refs #11 Added calculation of wavelength_to_energy_test

--- trunk/axis_manip/test/cpp/wavelength_to_energy_test.cpp
+++ trunk/axis_manip/test/cpp/wavelength_to_energy_test.cpp
@@ -7,17 +7,17 @@
  *
  *
  *                             NOTICE
- * 
- * For this software and its associated documentation, permission is granted 
- * to reproduce, prepare derivative works, and distribute copies to the public 
+ *
+ * For this software and its associated documentation, permission is granted
+ * to reproduce, prepare derivative works, and distribute copies to the public
  * for any purpose and without fee.
- *  
- * This material was prepared as an account of work sponsored by an agency of 
- * the United States Government.  Neither the United States Government nor the 
- * United States Department of Energy, nor any of their employees, makes any 
- * warranty, express or implied, or assumes any legal liability or 
- * responsibility for the accuracy, completeness, or usefulness of any 
- * information, apparatus, product, or process disclosed, or represents that 
+ *
+ * This material was prepared as an account of work sponsored by an agency of
+ * the United States Government.  Neither the United States Government nor the
+ * United States Department of Energy, nor any of their employees, makes any
+ * warranty, express or implied, or assumes any legal liability or
+ * responsibility for the accuracy, completeness, or usefulness of any
+ * information, apparatus, product, or process disclosed, or represents that
  * its use would not infringe privately owned rights.
  *
  */
@@ -53,36 +53,55 @@ void initialize_inputs(Nessi::Vector<NumT> & wavelength,
                        Nessi::Vector<NumT> & wavelength_err2){
   for( size_t i=0 ; i<NUM_VAL ; i++ )
     {
-      wavelength.push_back(static_cast<NumT>(0.));
-      wavelength_err2.push_back(static_cast<NumT>(0.));
+      wavelength.push_back(static_cast<NumT>(i+1));
+      wavelength_err2.push_back(static_cast<NumT>(i+0.5));
     }
 }
 
 /**
  * This function generate the values to compare the calculation to.
  */
-template <typename NumT>
-void initialize_true_outputs(NumT    & true_output_ss,
-           NumT    & true_output_ss_err2,
-           Nessi::Vector<NumT> & true_output_vv,
-           Nessi::Vector<NumT> & true_output_vv_err2){
+void initialize_true_outputs(float    & true_output_ss,
+                             float    & true_output_ss_err2,
+                             Nessi::Vector<float> & true_output_vv,
+                             Nessi::Vector<float> & true_output_vv_err2){
   // scalar scalar
-  true_output_ss=static_cast<NumT>(0.);
-  true_output_ss_err2=static_cast<NumT>(0.);
-  
+  true_output_ss=static_cast<float>(81.80420684814453125);
+  true_output_ss_err2=static_cast<float>(13383.8564453125);
+
   // vector vector
-  true_output_vv.push_back(static_cast<NumT>(0.));
-  true_output_vv_err2.push_back(static_cast<NumT>(0.));
-  true_output_vv.push_back(static_cast<NumT>(0.));
-  true_output_vv_err2.push_back(static_cast<NumT>(0.));
-  true_output_vv.push_back(static_cast<NumT>(0.));
-  true_output_vv_err2.push_back(static_cast<NumT>(0.));
-  true_output_vv.push_back(static_cast<NumT>(0.));
-  true_output_vv_err2.push_back(static_cast<NumT>(0.));
-  true_output_vv.push_back(static_cast<NumT>(0.));
-  true_output_vv_err2.push_back(static_cast<NumT>(0.));
+  true_output_vv.push_back(static_cast<float>(81.80420684814453125));
+  true_output_vv_err2.push_back(static_cast<float>(13383.8564453125));
+  true_output_vv.push_back(static_cast<float>(20.4510517120361328125));
+  true_output_vv_err2.push_back(static_cast<float>(627.3682861328125));
+  true_output_vv.push_back(static_cast<float>(9.08935642242431640625));
+  true_output_vv_err2.push_back(static_cast<float>(91.79599761962890625));
+  true_output_vv.push_back(static_cast<float>(5.112762928009033203125));
+  true_output_vv_err2.push_back(static_cast<float>(22.8728008270263671875));
+  true_output_vv.push_back(static_cast<float>(3.27216815948486328125));
+  true_output_vv_err2.push_back(static_cast<float>(7.70910167694091796875));
 }
 
+void initialize_true_outputs(double    & true_output_ss,
+                             double    & true_output_ss_err2,
+                             Nessi::Vector<double> & true_output_vv,
+                             Nessi::Vector<double> & true_output_vv_err2){
+  // scalar scalar
+  true_output_ss=static_cast<double>(81.804205100000004335925041232258);
+  true_output_ss_err2=static_cast<double>(13383.855944085733426618389785289764);
+
+  // vector vector
+  true_output_vv.push_back(static_cast<double>(81.804205100000004335925041232258));
+  true_output_vv_err2.push_back(static_cast<double>(13383.855944085733426618389785289764));
+  true_output_vv.push_back(static_cast<double>(20.451051275000001083981260308065));
+  true_output_vv_err2.push_back(static_cast<double>(627.368247379018725951027590781450));
+  true_output_vv.push_back(static_cast<double>(9.089356122222222111872724781279));
+  true_output_vv_err2.push_back(static_cast<double>(91.795994129531777616648469120264));
+  true_output_vv.push_back(static_cast<double>(5.112762818750000270995315077016));
+  true_output_vv_err2.push_back(static_cast<double>(22.872800685693391642416827380657));
+  true_output_vv.push_back(static_cast<double>(3.272168204000000191200570043293));
+  true_output_vv_err2.push_back(static_cast<double>(7.709101023793382800874951499281));
+}
 /**
  * This functions compares the expected with calculated values.
  */
@@ -123,8 +142,8 @@ bool test_func(NumT key){ // key forces correct test to happen
   NumT                  output_ss_err2;
   NumT                  true_output_ss;
   NumT                  true_output_ss_err2;
-  Nessi::Vector<NumT>   output_vv;
-  Nessi::Vector<NumT>   output_vv_err2;
+  Nessi::Vector<NumT>   output_vv(5);
+  Nessi::Vector<NumT>   output_vv_err2(5);
   Nessi::Vector<NumT>   true_output_vv;
   Nessi::Vector<NumT>   true_output_vv_err2;
 
