2d86f075f2afb30918eed00f5cb3153f2a0585dd
j35 <j35@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-01-25 14:09:36 +0000

    Version without white spaces

--- trunk/axis_manip/test/cpp/tof_to_initial_wavelength_igs_test.cpp
+++ trunk/axis_manip/test/cpp/tof_to_initial_wavelength_igs_test.cpp
@@ -7,17 +7,17 @@
  *
  *
  *           NOTICE
- * 
- * For this software and its associated documentation, permission is granted 
- * to reproduce, prepare derivative works, and distribute copies to the public 
+ *
+ * For this software and its associated documentation, permission is granted
+ * to reproduce, prepare derivative works, and distribute copies to the public
  * for any purpose and without fee.
- *  
- * This material was prepared as an account of work sponsored by an agency of 
- * the United States Government.  Neither the United States Government nor the 
- * United States Department of Energy, nor any of their employees, makes any 
- * warranty, express or implied, or assumes any legal liability or 
- * responsibility for the accuracy, completeness, or usefulness of any 
- * information, apparatus, product, or process disclosed, or represents that 
+ *
+ * This material was prepared as an account of work sponsored by an agency of
+ * the United States Government.  Neither the United States Government nor the
+ * United States Department of Energy, nor any of their employees, makes any
+ * warranty, express or implied, or assumes any legal liability or
+ * responsibility for the accuracy, completeness, or usefulness of any
+ * information, apparatus, product, or process disclosed, or represents that
  * its use would not infringe privately owned rights.
  *
  */
@@ -69,7 +69,7 @@ void initialize_true_outputs(NumT    & true_output_ss,
   // scalar scalar
   true_output_ss=static_cast<NumT>(-0.58583914499999989456);
   true_output_ss_err2=static_cast<NumT>(0.14668620920470620828);
-  
+
   // vector vector
   true_output_vv.push_back(static_cast<NumT>(-0.58583914499999989456));
   true_output_vv_err2.push_back(static_cast<NumT>(0.14668620920470620828));
@@ -125,8 +125,8 @@ bool test_func(NumT key){ // key forces correct test to happen
   NumT                  time_offset_err2=static_cast<NumT>(0.75);
   NumT                  dist_source_sample=static_cast<NumT>(1.1);
   NumT                  dist_source_sample_err2=static_cast<NumT>(0.1);
-  NumT  	        dist_sample_detector=static_cast<NumT>(0.51);
-  NumT  	        dist_sample_detector_err2=static_cast<NumT>(0.05);
+  NumT            dist_sample_detector=static_cast<NumT>(0.51);
+  NumT            dist_sample_detector_err2=static_cast<NumT>(0.05);
   NumT                  output_ss;
   NumT                  output_ss_err2;
   NumT                  true_output_ss;
