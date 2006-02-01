c4f7c5d1736fa3e92e4c88bc296612164277a7ad
2zr <mareuternh@gmail.com>
2006-02-01 15:52:27 +0000

    Initial prototype of debugging mechanism for tests. This refs #35.

--- axis_manip/test/cpp/test_common.hpp
+++ axis_manip/test/cpp/test_common.hpp
@@ -37,6 +37,10 @@ const std::string SV("s,v");
 const std::string VS("v,s");
 const std::string VV("v,v");
 
+// Debugging level strings
+const std::string ALL("all");
+const std::string INFO("info");
+
 template <typename NumT>
 std::string type_string(Nessi::Vector<NumT> & vec)
 {
@@ -78,8 +82,8 @@ template <typename NumT>
 bool test_okay(Nessi::Vector<NumT> & output,
                Nessi::Vector<NumT> & true_output,
                std::string array_type,
-               std::string data_type=EMPTY){
-
+               std::string data_type=EMPTY)
+{
   if(!Utils::vector_is_equals(output,true_output))
     {
       std::cout << "(" << type_string(output) << " " << array_type
@@ -95,8 +99,8 @@ template <typename NumT>
 bool test_okay(NumT & output,
                NumT & true_output,
                std::string array_type=SS,
-               std::string data_type=EMPTY){
-
+               std::string data_type=EMPTY)
+{
   if(Utils::compare(output,true_output)!=0)
     {
       std::cout << "(" << type_string(output) << " " << array_type
@@ -115,4 +119,39 @@ bool test_okay(NumT & output,
       return false;
     }
   return true;
+}
+
+// Print out two arrays side-by-side to check for inconsitencies
+template <typename NumT>
+void print(Nessi::Vector<NumT> & array1,
+           Nessi::Vector<NumT> & array2,
+           std::string array_type,
+           std::string & level)
+{
+  std::cout << "Printing " << type_string(array1) << " arrays for ";
+  std::cout << array_type << "." << std::endl;
+
+  std::cout.setf(std::ios::fixed);
+  std::cout << std::setprecision(std::numeric_limits<NumT>::digits10+1);
+
+  size_t size = array1.size();
+  for(size_t i = 0; i < size; ++i)
+    {
+      std::cout << array1[i] << "\t" << array2[i] << std::endl;
+    }
+}
+
+// Print out two arrays side-by-side to check for inconsitencies
+template <typename NumT>
+void print(NumT & value1,
+           NumT & value2,
+           std::string array_type,
+           std::string & level)
+{
+  std::cout << "Printing " << type_string(value1) << " values for ";
+  std::cout << array_type << "." << std::endl;
+
+  std::cout.setf(std::ios::fixed);
+  std::cout << std::setprecision(std::numeric_limits<NumT>::digits10+1);
+  std::cout << value1 << "\t" << value2 << std::endl;
 }
