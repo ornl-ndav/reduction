290c4a100e9800afe97d6cf83a25f51b65fab4d3
2zr <mareuternh@gmail.com>
2006-02-17 13:54:43 +0000

    Parameter name cleanup.

--- bindings/test/PBL/test_sumw_ncerr.py
+++ bindings/test/PBL/test_sumw_ncerr.py
@@ -63,112 +63,112 @@ for counter in range(vec_length):
 ###############################################################################
 
 # Truth values for sumw_ncerr vector-vector version
-TruthOutput_SumwNcerr_F = FloatNessiVector()
-TruthOutput_SumwNcerr_F.append(6.0)
-TruthOutput_SumwNcerr_F.append(6.0)
-TruthOutput_SumwNcerr_F.append(6.0)
-TruthOutput_SumwNcerr_F.append(6.0)
-TruthOutput_SumwNcerr_F.append(6.0)
+TruthOutput_VV_F = FloatNessiVector()
+TruthOutput_VV_F.append(6.0)
+TruthOutput_VV_F.append(6.0)
+TruthOutput_VV_F.append(6.0)
+TruthOutput_VV_F.append(6.0)
+TruthOutput_VV_F.append(6.0)
 
-TruthOutput_SumwNcerr_D = DoubleNessiVector()
-TruthOutput_SumwNcerr_D.append(6.0)
-TruthOutput_SumwNcerr_D.append(6.0)
-TruthOutput_SumwNcerr_D.append(6.0)
-TruthOutput_SumwNcerr_D.append(6.0)
-TruthOutput_SumwNcerr_D.append(6.0)
+TruthOutput_VV_D = DoubleNessiVector()
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
 
-TruthOutput_SumwNcerr_I = IntNessiVector()
-TruthOutput_SumwNcerr_I.append(6)
-TruthOutput_SumwNcerr_I.append(6)
-TruthOutput_SumwNcerr_I.append(6)
-TruthOutput_SumwNcerr_I.append(6)
-TruthOutput_SumwNcerr_I.append(6)
+TruthOutput_VV_I = IntNessiVector()
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
 
-TruthOutput_SumwNcerr_U = UnsignedIntNessiVector()
-TruthOutput_SumwNcerr_U.append(6)
-TruthOutput_SumwNcerr_U.append(6)
-TruthOutput_SumwNcerr_U.append(6)
-TruthOutput_SumwNcerr_U.append(6)
-TruthOutput_SumwNcerr_U.append(6)
+TruthOutput_VV_U = UnsignedIntNessiVector()
+TruthOutput_VV_U.append(6)
+TruthOutput_VV_U.append(6)
+TruthOutput_VV_U.append(6)
+TruthOutput_VV_U.append(6)
+TruthOutput_VV_U.append(6)
 
-TruthOutput_Err2_SumwNcerr_F = FloatNessiVector()
-TruthOutput_Err2_SumwNcerr_F.append(4.0)
-TruthOutput_Err2_SumwNcerr_F.append(4.0)
-TruthOutput_Err2_SumwNcerr_F.append(4.0)
-TruthOutput_Err2_SumwNcerr_F.append(4.0)
-TruthOutput_Err2_SumwNcerr_F.append(4.0)
+TruthOutput_Err2_VV_F = FloatNessiVector()
+TruthOutput_Err2_VV_F.append(4.0)
+TruthOutput_Err2_VV_F.append(4.0)
+TruthOutput_Err2_VV_F.append(4.0)
+TruthOutput_Err2_VV_F.append(4.0)
+TruthOutput_Err2_VV_F.append(4.0)
 
-TruthOutput_Err2_SumwNcerr_D = DoubleNessiVector()
-TruthOutput_Err2_SumwNcerr_D.append(4.0)
-TruthOutput_Err2_SumwNcerr_D.append(4.0)
-TruthOutput_Err2_SumwNcerr_D.append(4.0)
-TruthOutput_Err2_SumwNcerr_D.append(4.0)
-TruthOutput_Err2_SumwNcerr_D.append(4.0)
+TruthOutput_Err2_VV_D = DoubleNessiVector()
+TruthOutput_Err2_VV_D.append(4.0)
+TruthOutput_Err2_VV_D.append(4.0)
+TruthOutput_Err2_VV_D.append(4.0)
+TruthOutput_Err2_VV_D.append(4.0)
+TruthOutput_Err2_VV_D.append(4.0)
 
-TruthOutput_Err2_SumwNcerr_I = IntNessiVector()
-TruthOutput_Err2_SumwNcerr_I.append(4)
-TruthOutput_Err2_SumwNcerr_I.append(4)
-TruthOutput_Err2_SumwNcerr_I.append(4)
-TruthOutput_Err2_SumwNcerr_I.append(4)
-TruthOutput_Err2_SumwNcerr_I.append(4)
+TruthOutput_Err2_VV_I = IntNessiVector()
+TruthOutput_Err2_VV_I.append(4)
+TruthOutput_Err2_VV_I.append(4)
+TruthOutput_Err2_VV_I.append(4)
+TruthOutput_Err2_VV_I.append(4)
+TruthOutput_Err2_VV_I.append(4)
 
-TruthOutput_Err2_SumwNcerr_U = UnsignedIntNessiVector()
-TruthOutput_Err2_SumwNcerr_U.append(4)
-TruthOutput_Err2_SumwNcerr_U.append(4)
-TruthOutput_Err2_SumwNcerr_U.append(4)
-TruthOutput_Err2_SumwNcerr_U.append(4)
-TruthOutput_Err2_SumwNcerr_U.append(4)
+TruthOutput_Err2_VV_U = UnsignedIntNessiVector()
+TruthOutput_Err2_VV_U.append(4)
+TruthOutput_Err2_VV_U.append(4)
+TruthOutput_Err2_VV_U.append(4)
+TruthOutput_Err2_VV_U.append(4)
+TruthOutput_Err2_VV_U.append(4)
 
 ###############################################################################
 # Create output placeholders for vectors
 ###############################################################################
 
 # Output placeholders for sumw_ncerr vector-vector version
-Output_SumwNcerr_F = FloatNessiVector(len(Input1_F))
-Output_SumwNcerr_D = DoubleNessiVector(len(Input1_D))
-Output_SumwNcerr_I = IntNessiVector(len(Input1_I))
-Output_SumwNcerr_U = UnsignedIntNessiVector(len(Input1_U))
-Output_Err2_SumwNcerr_F = FloatNessiVector(len(Input1_Err2_F))
-Output_Err2_SumwNcerr_D = DoubleNessiVector(len(Input1_Err2_D))
-Output_Err2_SumwNcerr_I = IntNessiVector(len(Input1_Err2_I))
-Output_Err2_SumwNcerr_U = UnsignedIntNessiVector(len(Input1_Err2_U))
+Output_VV_F = FloatNessiVector(len(Input1_F))
+Output_VV_D = DoubleNessiVector(len(Input1_D))
+Output_VV_I = IntNessiVector(len(Input1_I))
+Output_VV_U = UnsignedIntNessiVector(len(Input1_U))
+Output_Err2_VV_F = FloatNessiVector(len(Input1_Err2_F))
+Output_Err2_VV_D = DoubleNessiVector(len(Input1_Err2_D))
+Output_Err2_VV_I = IntNessiVector(len(Input1_Err2_I))
+Output_Err2_VV_U = UnsignedIntNessiVector(len(Input1_Err2_U))
 
 print "Checking Vector-Vector Sum Weighted by Uncertainties Binding Function"
 
 sumw_ncerr_f(Input1_F, Input1_Err2_F, Input2_F, Input2_Err2_F,
-            Output_SumwNcerr_F, Output_Err2_SumwNcerr_F)
+            Output_VV_F, Output_Err2_VV_F)
 
-mess = test_common_bind.makeCheck("sumw_ncerr_f", Output_SumwNcerr_F,
-                                  TruthOutput_SumwNcerr_F,
-                                  Output_Err2_SumwNcerr_F,
-                                  TruthOutput_Err2_SumwNcerr_F)
+mess = test_common_bind.makeCheck("sumw_ncerr_f", Output_VV_F,
+                                  TruthOutput_VV_F,
+                                  Output_Err2_VV_F,
+                                  TruthOutput_Err2_VV_F)
 print mess
 
 sumw_ncerr_d(Input1_D, Input1_Err2_D, Input2_D, Input2_Err2_D,
-            Output_SumwNcerr_D, Output_Err2_SumwNcerr_D)
+            Output_VV_D, Output_Err2_VV_D)
 
-mess = test_common_bind.makeCheck("sumw_ncerr_d", Output_SumwNcerr_D,
-                                  TruthOutput_SumwNcerr_D,
-                                  Output_Err2_SumwNcerr_D,
-                                  TruthOutput_Err2_SumwNcerr_D)
+mess = test_common_bind.makeCheck("sumw_ncerr_d", Output_VV_D,
+                                  TruthOutput_VV_D,
+                                  Output_Err2_VV_D,
+                                  TruthOutput_Err2_VV_D)
 print mess
 
 sumw_ncerr_i(Input1_I, Input1_Err2_I, Input2_I, Input2_Err2_I,
-            Output_SumwNcerr_I, Output_Err2_SumwNcerr_I)
+            Output_VV_I, Output_Err2_VV_I)
 
-mess = test_common_bind.makeCheck("sumw_ncerr_i", Output_SumwNcerr_I,
-                                  TruthOutput_SumwNcerr_I,
-                                  Output_Err2_SumwNcerr_I,
-                                  TruthOutput_Err2_SumwNcerr_I)
+mess = test_common_bind.makeCheck("sumw_ncerr_i", Output_VV_I,
+                                  TruthOutput_VV_I,
+                                  Output_Err2_VV_I,
+                                  TruthOutput_Err2_VV_I)
 print mess
 
 sumw_ncerr_u(Input1_U, Input1_Err2_U, Input2_U, Input2_Err2_U,
-            Output_SumwNcerr_U, Output_Err2_SumwNcerr_U)
+            Output_VV_U, Output_Err2_VV_U)
 
-mess = test_common_bind.makeCheck("sumw_ncerr_u", Output_SumwNcerr_U,
-                                  TruthOutput_SumwNcerr_U,
-                                  Output_Err2_SumwNcerr_U,
-                                  TruthOutput_Err2_SumwNcerr_U)
+mess = test_common_bind.makeCheck("sumw_ncerr_u", Output_VV_U,
+                                  TruthOutput_VV_U,
+                                  Output_Err2_VV_U,
+                                  TruthOutput_Err2_VV_U)
 print mess
 
 
