d58eeae9ceae72e7f8fa438e082095f242f11153
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-03-08 14:56:17 +0000

    Fixing smoke tests to reflect devolved PBL interface. This refs #53.

--- trunk/bindings/test/PBL/test_init_scatt_wavevector_to_scalar_Q.py
+++ trunk/bindings/test/PBL/test_init_scatt_wavevector_to_scalar_Q.py
@@ -7,7 +7,6 @@
 #
 ###############################################################################
 
-from axis_manip_bind import init_scatt_wavevector_to_scalar_Q_f
 from axis_manip_bind import init_scatt_wavevector_to_scalar_Q_d
 from nessi_vector_bind import *
 import test_common_bind
@@ -26,23 +25,15 @@ polar_angle = 0.22
 polar_angle_err2 = 0.11
 
 # Create input vectors
-Input1_F = FloatNessiVector()
-Input1_Err2_F = FloatNessiVector()
 Input1_D = DoubleNessiVector()
 Input1_Err2_D = DoubleNessiVector()
-Input2_F = FloatNessiVector()
-Input2_Err2_F = FloatNessiVector()
 Input2_D = DoubleNessiVector()
 Input2_Err2_D = DoubleNessiVector()
 
 # Place values in the input vectors
 for counter in range(NUM_VAL):
-    Input1_F.append(float(counter+1))
-    Input1_Err2_F.append(float(counter+0.5))
     Input1_D.append(float(counter+1))
     Input1_Err2_D.append(float(counter+0.5))
-    Input2_F.append(float(counter+1.25))
-    Input2_Err2_F.append(float(counter+0.25))
     Input2_D.append(float(counter+1.25))
     Input2_Err2_D.append(float(counter+0.25))
 
@@ -51,26 +42,12 @@ for counter in range(NUM_VAL):
 ###############################################################################
 
 # Truth values for init_scatt_wavevector_to_scalar_Q vector-vector version
-TruthOutput_VV_F = FloatNessiVector()
-TruthOutput_VV_F.append(0.3503661155)
-TruthOutput_VV_F.append(0.5286048650)
-TruthOutput_VV_F.append(0.7297254800)
-TruthOutput_VV_F.append(0.9391418099)
-TruthOutput_VV_F.append(1.1523394584)
-
 TruthOutput_VV_D = DoubleNessiVector()
 TruthOutput_VV_D.append(0.35036606096122696)
 TruthOutput_VV_D.append(0.52860472569260141)
 TruthOutput_VV_D.append(0.72972579648330349)
 TruthOutput_VV_D.append(0.93914148175842626)
 TruthOutput_VV_D.append(1.15233845294826764)
-
-TruthOutput_Err2_VV_F = FloatNessiVector()
-TruthOutput_Err2_VV_F.append(0.4165994822)
-TruthOutput_Err2_VV_F.append(0.9832010269)
-TruthOutput_Err2_VV_F.append(1.512506723)
-TruthOutput_Err2_VV_F.append(2.2451405525)
-TruthOutput_Err2_VV_F.append(3.2094576355)
 
 TruthOutput_Err2_VV_D = DoubleNessiVector()
 TruthOutput_Err2_VV_D.append(0.41659964378851194)
@@ -80,26 +57,12 @@ TruthOutput_Err2_VV_D.append(2.24514211944373531)
 TruthOutput_Err2_VV_D.append(3.20946303556517609)
 
 # Truth values for init_scatt_wavevector_to_scalar_Q vector-scalar version
-TruthOutput_VS_F = FloatNessiVector()
-TruthOutput_VS_F.append(0.3503661155)
-TruthOutput_VS_F.append(0.8264459371)
-TruthOutput_VS_F.append(1.8009078502)
-TruthOutput_VS_F.append(2.7934792041)
-TruthOutput_VS_F.append(3.7899582386)
-
 TruthOutput_VS_D = DoubleNessiVector()
 TruthOutput_VS_D.append(0.35036606096122696)
 TruthOutput_VS_D.append(0.82644585627067690)
 TruthOutput_VS_D.append(1.80090786272381464)
 TruthOutput_VS_D.append(2.79347910439543900)
 TruthOutput_VS_D.append(3.78995803187415658)
-
-TruthOutput_Err2_VS_F = FloatNessiVector()
-TruthOutput_Err2_VS_F.append(0.4165994822)
-TruthOutput_Err2_VS_F.append(1.5647885799)
-TruthOutput_Err2_VS_F.append(2.6823163032)
-TruthOutput_Err2_VS_F.append(3.7089958190)
-TruthOutput_Err2_VS_F.append(4.7202110290)
 
 TruthOutput_Err2_VS_D = DoubleNessiVector()
 TruthOutput_Err2_VS_D.append(0.41659964378851194)
@@ -109,26 +72,12 @@ TruthOutput_Err2_VS_D.append(3.70899611257809613)
 TruthOutput_Err2_VS_D.append(4.72021164820286465)
 
 # Truth values for init_scatt_wavevector_to_scalar_Q scalar-vector version
-TruthOutput_SV_F = FloatNessiVector()
-TruthOutput_SV_F.append(0.3503661155)
-TruthOutput_SV_F.append(1.2926567518)
-TruthOutput_SV_F.append(2.2845494747)
-TruthOutput_SV_F.append(3.2813673019)
-TruthOutput_SV_F.append(4.2796702384)
-
 TruthOutput_SV_D = DoubleNessiVector()
 TruthOutput_SV_D.append(0.35036606096122696)
 TruthOutput_SV_D.append(1.29265675181475581)
 TruthOutput_SV_D.append(2.28454953532442894)
 TruthOutput_SV_D.append(3.28136734924480144)
 TruthOutput_SV_D.append(4.27967017210773370)
-
-TruthOutput_Err2_SV_F = FloatNessiVector()
-TruthOutput_Err2_SV_F.append(0.4165994822)
-TruthOutput_Err2_SV_F.append(1.6581019042)
-TruthOutput_Err2_SV_F.append(2.6918807029)
-TruthOutput_Err2_SV_F.append(3.7044682502)
-TruthOutput_Err2_SV_F.append(4.7109990119)
 
 TruthOutput_Err2_SV_D = DoubleNessiVector()
 TruthOutput_Err2_SV_D.append(0.41659964378851194)
@@ -138,11 +87,7 @@ TruthOutput_Err2_SV_D.append(3.70446789959436140)
 TruthOutput_Err2_SV_D.append(4.71099862050503759)
 
 # Truth values for init_scatt_wavevector_to_scalar_Q scalar-scalar version
-TruthOutput_SS_F = 0.3503661155
-
 TruthOutput_SS_D = 0.35036606096122696
-
-TruthOutput_Err2_SS_F = 0.4165994822
 
 TruthOutput_Err2_SS_D = 0.41659964378851194
 
@@ -152,46 +97,26 @@ TruthOutput_Err2_SS_D = 0.41659964378851194
 
 # Output placeholders for init_scatt_wavevector_to_scalar_Q vector-vector
 # version
-Output_VV_F = FloatNessiVector(len(Input1_F))
 Output_VV_D = DoubleNessiVector(len(Input1_D))
-Output_Err2_VV_F = FloatNessiVector(len(Input1_Err2_F))
 Output_Err2_VV_D = DoubleNessiVector(len(Input1_Err2_D))
 
 # Output placeholders for init_scatt_wavevector_to_scalar_Q vector-scalar
 # version
-Output_VS_F = FloatNessiVector(len(Input1_F))
 Output_VS_D = DoubleNessiVector(len(Input1_D))
-Output_Err2_VS_F = FloatNessiVector(len(Input1_Err2_F))
 Output_Err2_VS_D = DoubleNessiVector(len(Input1_Err2_D))
 
 # Output placeholders for init_scatt_wavevector_to_scalar_Q scalar-vector
 # version
-Output_SV_F = FloatNessiVector(len(Input1_F))
 Output_SV_D = DoubleNessiVector(len(Input1_D))
-Output_Err2_SV_F = FloatNessiVector(len(Input1_Err2_F))
 Output_Err2_SV_D = DoubleNessiVector(len(Input1_Err2_D))
 
 # Output placeholders for init_scatt_wavevector_to_scalar_Q scalar-scalar
 # version
-Output_SS_F = 0.0
 Output_SS_D = 0.0
-Output_Err2_SS_F = 0.0
 Output_Err2_SS_D = 0.0
 
 print "Checking Vector-Vector Initial and Scattering Wavevector to Scalar Q"
 print "Binding Function"
-
-init_scatt_wavevector_to_scalar_Q_f(Input1_F, Input1_Err2_F,
-                                    Input2_F, Input2_Err2_F,
-                                    polar_angle, polar_angle_err2,
-                                    Output_VV_F, Output_Err2_VV_F)
-
-mess = test_common_bind.makeCheck("init_scatt_wavevector_to_scalar_Q_f",
-                                  Output_VV_F,
-                                  TruthOutput_VV_F,
-                                  Output_Err2_VV_F,
-                                  TruthOutput_Err2_VV_F)
-print mess
 
 init_scatt_wavevector_to_scalar_Q_d(Input1_D, Input1_Err2_D,
                                     Input2_D, Input2_Err2_D,
@@ -208,18 +133,6 @@ print
 print "Checking Vector-Scalar Initial and Scattering Wavevector to Scalar Q"
 print "Binding Function"
 
-init_scatt_wavevector_to_scalar_Q_f(Input1_F, Input1_Err2_F,
-                                    Input2_F[0], Input2_Err2_F[0],
-                                    polar_angle, polar_angle_err2,
-                                    Output_VS_F, Output_Err2_VS_F)
-
-mess = test_common_bind.makeCheck("init_scatt_wavevector_to_scalar_Q_f",
-                                  Output_VS_F,
-                                  TruthOutput_VS_F,
-                                  Output_Err2_VS_F,
-                                  TruthOutput_Err2_VS_F)
-print mess
-
 init_scatt_wavevector_to_scalar_Q_d(Input1_D, Input1_Err2_D,
                                     Input2_D[0], Input2_Err2_D[0],
                                     polar_angle, polar_angle_err2,
@@ -235,18 +148,6 @@ print
 print "Checking Scalar-Vector Initial and Scattering Wavevector to Scalar Q"
 print "Binding Function"
 
-init_scatt_wavevector_to_scalar_Q_f(Input1_F[0], Input1_Err2_F[0],
-                                    Input2_F, Input2_Err2_F,
-                                    polar_angle, polar_angle_err2,
-                                    Output_SV_F, Output_Err2_SV_F)
-
-mess = test_common_bind.makeCheck("init_scatt_wavevector_to_scalar_Q_f",
-                                  Output_SV_F,
-                                  TruthOutput_SV_F,
-                                  Output_Err2_SV_F,
-                                  TruthOutput_Err2_SV_F)
-print mess
-
 init_scatt_wavevector_to_scalar_Q_d(Input1_D[0], Input1_Err2_D[0],
                                     Input2_D, Input2_Err2_D,
                                     polar_angle, polar_angle_err2,
@@ -261,18 +162,6 @@ print mess
 print
 print "Checking Scalar-Scalar Initial and Scattering Wavevector to Scalar Q"
 print "Binding Function"
-
-init_scatt_wavevector_to_scalar_Q_f(Input1_F[0], Input1_Err2_F[0],
-                                    Input2_F[0], Input2_Err2_F[0],
-                                    polar_angle, polar_angle_err2,
-                                    Output_SS_F, Output_Err2_SS_F)
-
-#mess = test_common_bind.makeCheck("init_scatt_wavevector_to_scalar_Q_f",
-#Output_SS_F,
-#                                  TruthOutput_SS_F,
-#                                  Output_Err2_SS_F,
-#                                  TruthOutput_Err2_SS_F)
-#print mess
 
 init_scatt_wavevector_to_scalar_Q_d(Input1_D[0], Input1_Err2_D[0],
                                     Input2_D[0], Input2_Err2_D[0],
--- trunk/bindings/test/PBL/test_wavelength_to_scalar_k.py
+++ trunk/bindings/test/PBL/test_wavelength_to_scalar_k.py
@@ -6,7 +6,6 @@
 #
 ###############################################################################
 
-from axis_manip_bind import wavelength_to_scalar_k_f
 from axis_manip_bind import wavelength_to_scalar_k_d
 from nessi_vector_bind import *
 import test_common_bind
@@ -20,15 +19,11 @@ print
 NUM_VAL = 5
 
 # Create input vectors
-Input_F = FloatNessiVector()
-Input_Err2_F = FloatNessiVector()
 Input_D = DoubleNessiVector()
 Input_Err2_D = DoubleNessiVector()
 
 # Place values in the input vectors
 for counter in range(NUM_VAL):
-    Input_F.append(float(counter+1))
-    Input_Err2_F.append(float(counter+0.5))
     Input_D.append(float(counter+1))
     Input_Err2_D.append(float(counter+0.5))
 
@@ -37,26 +32,12 @@ for counter in range(NUM_VAL):
 ###############################################################################
 
 # Truth values for wavelength_to_scalar_k vector-vector version
-TruthOutput_VV_F = FloatNessiVector()
-TruthOutput_VV_F.append(6.283185482)
-TruthOutput_VV_F.append(3.1415927410)
-TruthOutput_VV_F.append(2.0943951606)
-TruthOutput_VV_F.append(1.5707963705)
-TruthOutput_VV_F.append(1.2566370964)
-
 TruthOutput_VV_D = DoubleNessiVector()
 TruthOutput_VV_D.append(6.28318530717958623)
 TruthOutput_VV_D.append(3.14159265358979311)
 TruthOutput_VV_D.append(2.09439510239319526)
 TruthOutput_VV_D.append(1.57079632679489655)
 TruthOutput_VV_D.append(1.25663706143591724)
-
-TruthOutput_Err2_VV_F = FloatNessiVector()
-TruthOutput_Err2_VV_F.append(19.7392101287)
-TruthOutput_Err2_VV_F.append(3.70110177993)
-TruthOutput_Err2_VV_F.append(1.21846973896)
-TruthOutput_Err2_VV_F.append(0.53974401950)
-TruthOutput_Err2_VV_F.append(0.28424462676)
 
 TruthOutput_Err2_VV_D = DoubleNessiVector()
 TruthOutput_Err2_VV_D.append(19.73920880217871598)
@@ -66,11 +47,7 @@ TruthOutput_Err2_VV_D.append(0.539743990684574237)
 TruthOutput_Err2_VV_D.append(0.284244606751373529)
 
 # Truth values for wavelength_to_scalar_k scalar-scalar version
-TruthOutput_SS_F = 6.2831854820
-
 TruthOutput_SS_D = 6.28318530717958623
-
-TruthOutput_Err2_SS_F = 19.7392101287
 
 TruthOutput_Err2_SS_D = 19.73920880217871598
 
@@ -79,28 +56,14 @@ TruthOutput_Err2_SS_D = 19.73920880217871598
 ###############################################################################
 
 # Output placeholders for wavelength_to_scalar_k vector-vector version
-Output_VV_F = FloatNessiVector(len(Input_F))
 Output_VV_D = DoubleNessiVector(len(Input_D))
-Output_Err2_VV_F = FloatNessiVector(len(Input_Err2_F))
 Output_Err2_VV_D = DoubleNessiVector(len(Input_Err2_D))
 
 # Output placeholders for wavelength_to_scalar_k scalar-scalar version
-Output_SS_F = 0.0
 Output_SS_D = 0.0
-Output_Err2_SS_F = 0.0
 Output_Err2_SS_D = 0.0
 
 print "Checking Vector-Vector Wavelength to Scalar K Binding Function"
-
-wavelength_to_scalar_k_f(Input_F, Input_Err2_F,
-                         Output_VV_F, Output_Err2_VV_F)
-
-mess = test_common_bind.makeCheck("wavelength_to_scalar_k_f",
-                                  Output_VV_F,
-                                  TruthOutput_VV_F,
-                                  Output_Err2_VV_F,
-                                  TruthOutput_Err2_VV_F)
-print mess
 
 wavelength_to_scalar_k_d(Input_D, Input_Err2_D,
                          Output_VV_D, Output_Err2_VV_D)
@@ -113,16 +76,6 @@ mess = test_common_bind.makeCheck("wavelength_to_scalar_k_d",
 print mess
 print
 print "Checking Scalar-Scalar Wavelength to Scalar K Binding Function"
-
-wavelength_to_scalar_k_f(Input_F[0], Input_Err2_F[0],
-                         Output_SS_F, Output_Err2_SS_F)
-
-#mess = test_common_bind.makeCheck("wavelength_to_scalar_k_f",
-#Output_SS_F,
-#                                  TruthOutput_SS_F,
-#                                  Output_Err2_SS_F,
-#                                  TruthOutput_Err2_SS_F)
-#print mess
 
 wavelength_to_scalar_k_d(Input_D[0], Input_Err2_D[0],
                          Output_SS_D, Output_Err2_SS_D)
