da2b6c0530195ba2243148724e2f4fc921ae6e60
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-03-08 16:04:50 +0000

    Adding functionality the allows the weighted_average function to actually be run. This refs #53.

--- trunk/bindings/test/PBL/test_weighted_average.py
+++ trunk/bindings/test/PBL/test_weighted_average.py
@@ -8,6 +8,8 @@
 
 from nessi_vector_bind import *
 from utils_bind import weighted_average_d
+import test_common_bind
+from vpair_bind import *
 
 print "#################################################################"
 print "# Checking Weighted_Average Swig Generated Python Binding Layer #"
@@ -15,13 +17,13 @@ print "#################################################################"
 print
 
 # Set standard size for vectors
-vec_length = 5
+NUM_VAL = 5
 
 # Create input vectors
 Input1_D = DoubleNessiVector()
 Input1_Err2_D = DoubleNessiVector()
 
-for counter in range(vec_length):
+for counter in range(NUM_VAL):
     Input1_D.append(float(counter+1))
     Input1_Err2_D.append(float(1))
 
@@ -30,22 +32,28 @@ for counter in range(vec_length):
 ###############################################################################
 
 # Truth information for weighted_average
-truth_output_wa = float(3)
-truth_output_err2_wa = 0.2
+TruthOutput_D = DoubleVPair()
+TruthOutput_D.val=float(3)
+TruthOutput_D.val_err2=0.2
 
 ###############################################################################
 # Create output placeholders for vectors
 ###############################################################################
 
 # Output placeholders for weighted_average
-output_wa = 0.0
-output_err2_wa = 0.0
+Output_D = DoubleVPair()
 
 print "Checking Weighted Average Binding Function"
 
-print "weighted_average_d..........."
+weighted_average_d(Input1_D, Input1_Err2_D, 0, NUM_VAL-1, Output_D)
 
-weighted_average_d(Input1_D, Input1_Err2_D, 0, vec_length-1,
-                   output_wa, output_err2_wa)
+mess = test_common_bind.makeCheck2("weighted_average_d",
+                                   Output_D.val,
+                                   TruthOutput_D.val,
+                                   Output_D.val_err2,
+                                   TruthOutput_D.val_err2)
+
+print mess
+
 
 
