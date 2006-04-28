2e06d594c9d06de23694ebf258661b56807f1ba6
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-04-28 16:34:06 +0000

    Marked r947 as milestone:'1.0.0 ITC4 Ready'

---
+++ tags/1.0.0itc4/bindings/test/PBL/test_add_ncerr.py
@@ -0,0 +1,174 @@
+###############################################################################
+#
+# Testing script for Swig bindings for add_ncerr functions
+#
+# $Id$
+#
+###############################################################################
+
+##
+# \file bindings/test/PBL/test_add_ncerr.py
+#
+
+##
+# \defgroup test_add_ncerr pbl::test_add_ncerr
+# \{
+#
+
+from array_manip_bind import add_ncerr_d
+from array_manip_bind import add_ncerr_i
+from nessi_vector_bind import *
+import test_common_bind
+
+print "###########################################################"
+print "# Checking Add_Ncerr Swig Generated Python Binding Layer  #"
+print "###########################################################"
+print
+
+# Set standard size for vectors
+NUM_VAL = 5
+
+# Create input vectors
+Input1_D = DoubleNessiVector()
+Input1_Err2_D = DoubleNessiVector()
+Input1_I = IntNessiVector()
+Input1_Err2_I = IntNessiVector()
+Input2_D = DoubleNessiVector()
+Input2_Err2_D = DoubleNessiVector()
+Input2_I = IntNessiVector()
+Input2_Err2_I = IntNessiVector()
+
+# Place values in the input vectors
+for counter in range(NUM_VAL):
+    Input1_D.append(float(NUM_VAL+1-counter))
+    Input1_Err2_D.append(float(1))
+    Input1_I.append(NUM_VAL+1-counter)
+    Input1_Err2_I.append(1)
+    Input2_D.append(float(counter))
+    Input2_Err2_D.append(float(1))
+    Input2_I.append(counter)
+    Input2_Err2_I.append(1)
+
+###############################################################################
+# Create truth vectors and values
+###############################################################################
+
+# Truth values for add_ncerr vector-vector version
+TruthOutput_VV_D = DoubleNessiVector()
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+TruthOutput_VV_D.append(6.0)
+
+TruthOutput_VV_I = IntNessiVector()
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+TruthOutput_VV_I.append(6)
+
+TruthOutput_Err2_VV_D = DoubleNessiVector()
+TruthOutput_Err2_VV_D.append(2.0)
+TruthOutput_Err2_VV_D.append(2.0)
+TruthOutput_Err2_VV_D.append(2.0)
+TruthOutput_Err2_VV_D.append(2.0)
+TruthOutput_Err2_VV_D.append(2.0)
+
+TruthOutput_Err2_VV_I = IntNessiVector()
+TruthOutput_Err2_VV_I.append(2)
+TruthOutput_Err2_VV_I.append(2)
+TruthOutput_Err2_VV_I.append(2)
+TruthOutput_Err2_VV_I.append(2)
+TruthOutput_Err2_VV_I.append(2)
+
+# Truth values for add_ncerr vector-scalar version
+TruthOutput_VS_D = DoubleNessiVector()
+TruthOutput_VS_D.append(10.0)
+TruthOutput_VS_D.append(9.0)
+TruthOutput_VS_D.append(8.0)
+TruthOutput_VS_D.append(7.0)
+TruthOutput_VS_D.append(6.0)
+
+TruthOutput_VS_I = IntNessiVector()
+TruthOutput_VS_I.append(10)
+TruthOutput_VS_I.append(9)
+TruthOutput_VS_I.append(8)
+TruthOutput_VS_I.append(7)
+TruthOutput_VS_I.append(6)
+
+TruthOutput_Err2_VS_D = DoubleNessiVector()
+TruthOutput_Err2_VS_D.append(2.0)
+TruthOutput_Err2_VS_D.append(2.0)
+TruthOutput_Err2_VS_D.append(2.0)
+TruthOutput_Err2_VS_D.append(2.0)
+TruthOutput_Err2_VS_D.append(2.0)
+
+TruthOutput_Err2_VS_I = IntNessiVector()
+TruthOutput_Err2_VS_I.append(2)
+TruthOutput_Err2_VS_I.append(2)
+TruthOutput_Err2_VS_I.append(2)
+TruthOutput_Err2_VS_I.append(2)
+TruthOutput_Err2_VS_I.append(2)
+
+###############################################################################
+# Create output placeholders for vectors
+###############################################################################
+
+# Output placeholders for add_ncerr vector-vector version
+Output_VV_D = DoubleNessiVector(len(Input1_D))
+Output_VV_I = IntNessiVector(len(Input1_I))
+Output_Err2_VV_D = DoubleNessiVector(len(Input1_Err2_D))
+Output_Err2_VV_I = IntNessiVector(len(Input1_Err2_I))
+
+# Output placeholders for add_ncerr vector-scalar version
+Output_VS_D = DoubleNessiVector(len(Input1_D))
+Output_VS_I = IntNessiVector(len(Input1_I))
+Output_Err2_VS_D = DoubleNessiVector(len(Input1_Err2_D))
+Output_Err2_VS_I = IntNessiVector(len(Input1_Err2_I))
+
+print "Checking Vector-Vector Addition Binding Function"
+
+add_ncerr_d(Input1_D, Input1_Err2_D, Input2_D, Input2_Err2_D,
+            Output_VV_D, Output_Err2_VV_D)
+
+mess = test_common_bind.makeCheck("add_ncerr_d", Output_VV_D,
+                                  TruthOutput_VV_D,
+                                  Output_Err2_VV_D,
+                                  TruthOutput_Err2_VV_D)
+print mess
+
+add_ncerr_i(Input1_I, Input1_Err2_I, Input2_I, Input2_Err2_I,
+            Output_VV_I, Output_Err2_VV_I)
+
+mess = test_common_bind.makeCheck("add_ncerr_i", Output_VV_I,
+                                  TruthOutput_VV_I,
+                                  Output_Err2_VV_I,
+                                  TruthOutput_Err2_VV_I)
+print mess
+print
+print "Checking Vector-Scalar Addition Binding Function"
+
+add_ncerr_d(Input1_D, Input1_Err2_D,
+            Input2_D[NUM_VAL-1], Input2_Err2_D[NUM_VAL-1],
+            Output_VS_D, Output_Err2_VS_D)
+
+mess = test_common_bind.makeCheck("add_ncerr_d", Output_VS_D,
+                                  TruthOutput_VS_D,
+                                  Output_Err2_VS_D,
+                                  TruthOutput_Err2_VS_D)
+print mess
+
+add_ncerr_i(Input1_I, Input1_Err2_I,
+            Input2_I[NUM_VAL-1], Input2_Err2_I[NUM_VAL-1],
+            Output_VS_I, Output_Err2_VS_I)
+
+mess = test_common_bind.makeCheck("add_ncerr_i", Output_VS_I,
+                                  TruthOutput_VS_I,
+                                  Output_Err2_VS_I,
+                                  TruthOutput_Err2_VS_I)
+print mess
+
+##
+# \} // end of test_add_ncerr group
+#
---
+++ tags/1.0.0itc4/utils/src/compare.cpp
@@ -0,0 +1,106 @@
+/*
+ *                     SNS Common Libraries
+ *           A part of the SNS Analysis Software Suite.
+ *
+ *                  Spallation Neutron Source
+ *          Oak Ridge National Laboratory, Oak Ridge TN.
+ *
+ *
+ *                             NOTICE
+ *
+ * For this software and its associated documentation, permission is granted
+ * to reproduce, prepare derivative works, and distribute copies to the public
+ * for any purpose and without fee.
+ *
+ * This material was prepared as an account of work sponsored by an agency of
+ * the United States Government.  Neither the United States Government nor the
+ * United States Department of Energy, nor any of their employees, makes any
+ * warranty, express or implied, or assumes any legal liability or
+ * responsibility for the accuracy, completeness, or usefulness of any
+ * information, apparatus, product, or process disclosed, or represents that
+ * its use would not infringe privately owned rights.
+ *
+ */
+
+/**
+ * $Id$
+ *
+ * \file utils/src/compare.cpp
+ */
+#include "compare.hpp"
+#include <cmath>
+#include <limits>
+
+namespace Utils
+{
+  // specialized function for float
+
+  /**
+   * This is the float declaration of the compare function.
+   *
+   * \ingroup compare
+   */
+  int compare(const float & value1,
+              const float & value2)
+  {
+    if(!(fabs(value1 - value2) > std::numeric_limits<float>::epsilon()))
+      {
+        return 0;
+      }
+
+    if(value1 > value2)
+      {
+        return 1;
+      }
+    else
+      {
+        return -1;
+      }
+  }
+
+  // specialized function for double
+
+  /**
+   * This is the double declaration of the compare function.
+   *
+   * \ingroup compare
+   */
+  int compare(const double & value1,
+              const double & value2)
+  {
+    if(!(fabs(value1 - value2) > std::numeric_limits<double>::epsilon()))
+      {
+        return 0;
+      }
+
+    if(value1 > value2)
+      {
+        return 1;
+      }
+    else
+      {
+        return -1;
+      }
+  }
+
+  // explicit instantiations
+
+  /**
+   * This is the integer declaration of the compare function.
+   *
+   * \ingroup compare
+   */
+  template int
+  compare<int>(const int & value1,
+               const int & value2);
+
+  /**
+   * This is the unsigned integer declaration of the compare function.
+   *
+   * \ingroup compare
+   */
+  template int
+  compare<unsigned int>(const unsigned int & value1,
+                        const unsigned int & value2);
+
+} // Utils
