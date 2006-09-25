ea5ea66f24bdbea2f02b2c0249725bc42a9210b4
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2006-09-25 13:25:49 +0000

    Marked r1120 as milestone:'1.0.0 ITC5 Ready'

---
+++ tags/1.0.0itc5/axis_manip/src/tof_to_scalar_Q.hpp
@@ -0,0 +1,227 @@
+/*
+ *                     SNS Common Libraries
+ *           A part of the SNS Analysis Software Suite.
+ *
+ *                  Spallation Neutron Source
+ *          Oak Ridge National Laboratory, Oak Ridge TN.
+ *
+ *
+ *                             NOTICE
+ * 
+ * For this software and its associated documentation, permission is granted 
+ * to reproduce, prepare derivative works, and distribute copies to the public 
+ * for any purpose and without fee.
+ *  
+ * This material was prepared as an account of work sponsored by an agency of 
+ * the United States Government.  Neither the United States Government nor the 
+ * United States Department of Energy, nor any of their employees, makes any 
+ * warranty, express or implied, or assumes any legal liability or 
+ * responsibility for the accuracy, completeness, or usefulness of any 
+ * information, apparatus, product, or process disclosed, or represents that 
+ * its use would not infringe privately owned rights.
+ *
+ */
+
+/**
+ * $Id$
+ *
+ * \file axis_manip/src/tof_to_scalar_Q.hpp
+ */
+#ifndef _TOF_TO_SCALAR_Q_HPP
+#define _TOF_TO_SCALAR_Q_HPP 1
+
+#include "constants.hpp"
+#include "conversions.hpp"
+#include "nessi_warn.hpp"
+#include <cmath>
+#include <stdexcept>
+
+namespace AxisManip
+{
+  // 3.17
+  template <typename NumT>
+  std::string
+  tof_to_scalar_Q(const Nessi::Vector<NumT> & tof,
+                  const Nessi::Vector<NumT> & tof_err2,
+                  const NumT pathlength,
+                  const NumT pathlength_err2,
+                  const NumT scatt_angle,
+                  const NumT scatt_angle_err2,
+                  Nessi::Vector<NumT> & Q,
+                  Nessi::Vector<NumT> & Q_err2,
+                  void *temp=NULL)
+  {
+    std::string retstr(Nessi::EMPTY_WARN);
+    std::string warn;
+
+    NumT a;
+    NumT a2;
+    NumT sang;
+    NumT term1;
+    NumT term2;
+    NumT term3;
+
+    warn = __tof_to_scalar_Q_static(pathlength, pathlength_err2, scatt_angle,
+                                    scatt_angle_err2, a, a2, sang,
+                                    term1, term2, term3);
+    if (!warn.empty())
+      {
+        retstr += warn;
+      }
+
+    size_t size_tof = tof.size();
+    for (size_t i = 0; i < size_tof; ++i)
+      {
+        warn = __tof_to_scalar_Q_dynamic(tof[i], tof_err2[i], pathlength,
+                                         Q[i], Q_err2[i], a, a2, sang,
+                                         term1, term2, term3);
+
+        if (!warn.empty())
+          {
+            retstr += warn;
+          }
+      }
+
+
+
+    return retstr;
+  }
+
+  // 3.17
+  template <typename NumT>
+  std::string
+  tof_to_scalar_Q(const NumT tof,
+                  const NumT tof_err2,
+                  const NumT pathlength,
+                  const NumT pathlength_err2,
+                  const NumT scatt_angle,
+                  const NumT scatt_angle_err2,
+                  NumT & Q,
+                  NumT & Q_err2,
+                  void *temp=NULL)
+  {
+    std::string retstr(Nessi::EMPTY_WARN);
+    std::string warn;
+
+    NumT a;
+    NumT a2;
+    NumT sang;
+    NumT term1;
+    NumT term2;
+    NumT term3;
+
+    warn = __tof_to_scalar_Q_static(pathlength, pathlength_err2, scatt_angle,
+                                    scatt_angle_err2, a, a2, sang,
+                                    term1, term2, term3);
+    if (!warn.empty())
+      {
+        retstr += warn;
+      }
+
+    warn = __tof_to_scalar_Q_dynamic(tof, tof_err2, pathlength, Q, Q_err2,
+                                     a, a2, sang, term1, term2, term3);
+
+    if (!warn.empty())
+      {
+        retstr += warn;
+      }
+
+    return retstr;
+  }
+
+  /**
+   * \ingroup tof_to_scalar_Q
+   *
+   * This is a PRIVATE helper function for tof_to_scalar_Q that calculates the
+   * parameters invariant across the array calculation.
+   *
+   * \param pathlength (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param pathlength_err2 (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param scatt_angle (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param scatt_angle_err2 (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param a (OUTPUT) \f$=4\pi m_n/h\f$
+   * \param a2 (OUTPUT) \f$=a^2\f$
+   * \param sang (OUTPUT) \f$=cos(scatt\_angle)\f$
+   * \param term1 (OUTPUT) \f$=\cos^2(scatt\_angle)\times pathlength\_err2^2\f$
+   * \param term2 (OUTPUT) \f$=\cos^2(scatt\_angle)\times pathlength^2\times scatt\_angle\_err2^2\f$
+   * \param term3 (OUTPUT) \f$\cos^2(scatt\_angle)\times pathlength^2\f$
+   */
+  template <typename NumT>
+  std::string
+  __tof_to_scalar_Q_static(const NumT pathlength,
+                           const NumT pathlength_err2,
+                           const NumT scatt_angle,
+                           const NumT scatt_angle_err2,
+                           NumT & a,
+                           NumT & a2,
+                           NumT & sang,
+                           NumT & term1,
+                           NumT & term2,
+                           NumT & term3)
+  {
+    a = static_cast<NumT>(1./ PhysConst::H_OVER_MNEUT);
+    a *= static_cast<NumT>(4. * PhysConst::PI);
+    a2 = a * a;
+
+    // SNS-FIXME: Should one of these be sign? Change docs as well if so.
+    NumT cang = static_cast<NumT>(std::cos(static_cast<double>(scatt_angle)));
+    NumT cang2 = cang * cang;
+    sang = static_cast<NumT>(std::sin(static_cast<double>(scatt_angle)));
+    NumT sang2 = sang * sang;
+
+    term1 = sang2 * pathlength_err2 * pathlength_err2;
+    term2 = cang2 * pathlength * pathlength;
+    term2 *= scatt_angle_err2;
+
+    term3 = sang2 * pathlength * pathlength;
+
+    return std::string(Nessi::EMPTY_WARN);
+  }
+
+  /**
+   * \ingroup tof_to_scalar_Q
+   *
+   * This is a PRIVATE helper function for tof_to_scalar_Q that calculates the
+   * scalar_Q and its uncertainty.
+   *
+   * \param tof (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param tof_err2 (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param pathlength (INPUT) same as the parameter in tof_to_scalar_Q()
+   * \param Q (OUTPUT) same as the parameter in tof_to_scalar_Q()
+   * \param Q_err2 (OUTPUT) same as the parameter in tof_to_scalar_Q()
+   * \param a (INPUT) same as the parameter in __tof_to_scalar_Q_static()
+   * \param a2 (INPUT) same as the parameter in __tof_to_scalar_Q_static()
+   * \param sang (INPUT) same as the parameter in __tof_to_scalar_Q_static()
+   * \param term1 (INPUT) same as the parameter in __tof_to_scalar_Q_static()
+   * \param term2 (INPUT) same as the parameter in __tof_to_scalar_Q_static()
+   * \param term3 (INPUT) same as the parameter in __tof_to_scalar_Q_static()
+   */
+  template <typename NumT>
+  std::string
+  __tof_to_scalar_Q_dynamic(const NumT tof,
+                            const NumT tof_err2,
+                            const NumT pathlength,
+                            NumT & Q,
+                            NumT & Q_err2,
+                            const NumT a,
+                            const NumT a2,
+                            const NumT sang,
+                            const NumT term1,
+                            const NumT term2,
+                            const NumT term3)
+  {
+    Q = sang / tof;
+    Q *= (a * pathlength);
+
+    Q_err2 = tof_err2;
+    Q_err2 /= tof * tof;
+    Q_err2 *= term3;
+    Q_err2 += term1 + term2;
+    Q_err2 *= a2 / (tof * tof);
+
+    return Nessi::EMPTY_WARN;
+  }
+
+} // AxisManip
+
+#endif // _TOF_TO_SCALAR_Q_HPP
---
+++ tags/1.0.0itc5/bindings/test/PBL/test_d_spacing_to_tof_focused_det.py
@@ -0,0 +1,127 @@
+###############################################################################
+#
+# Testing script for Swig bindings for d_spacing_to_tof_focused_det functions
+#
+# $Id$
+#
+###############################################################################
+
+##
+# \file bindings/test/PBL/test_d_spacing_to_tof_focused_det.py
+#
+
+##
+# \defgroup test_d_spacing_to_tof_focused_det pbl::test_d_spacing_to_tof_focused_det
+# \{
+#
+
+from axis_manip_bind import d_spacing_to_tof_focused_det_d
+from axis_manip_bind import d_spacing_to_tof_focused_det_ss_d
+from nessi_vector_bind import *
+from os import uname
+from sys import platform
+import test_common_bind
+from vpair_bind import *
+
+print "##################################################################"
+print "# Checking D_Spacing_to_Tof_Focused_Det Swig Generated Python    #"
+print "# Binding Layer                                                  #"
+print "##################################################################"
+print
+
+# Set standard size for vectors
+NUM_VAL = 5
+
+# Set parameters for function
+pathlength_focused = 2.75;
+pathlength_focused_err2 = 0.75;
+polar_angle_focused = 4.0;
+polar_angle_focused_err2 = 0.5;
+
+# Create input vectors
+Input_D = DoubleNessiVector()
+Input_Err2_D = DoubleNessiVector()
+
+# Place values in the input vectors
+for counter in range(NUM_VAL):
+    Input_D.append(float(counter+1))
+    Input_Err2_D.append(float(counter+0.5))
+
+###############################################################################
+# Create truth vectors and values
+###############################################################################
+
+# Truth values for d_spacing_to_tof_focused_det vector-vector version
+TruthOutput_VV_D = DoubleNessiVector()
+TruthOutput_VV_D.append(1264.17918742388204636)
+TruthOutput_VV_D.append(2528.35837484776409272)
+TruthOutput_VV_D.append(3792.53756227164590728)
+TruthOutput_VV_D.append(5056.71674969552818102)
+TruthOutput_VV_D.append(6320.8959371194108650)
+
+TruthOutput_Err2_VV_D = DoubleNessiVector()
+if(platform=="linux2" and uname()[4]=="x86_64"):
+	TruthOutput_Err2_VV_D.append(999410.25272388576716936)
+else:
+	TruthOutput_Err2_VV_D.append(999410.2527238857001066)
+	
+TruthOutput_Err2_VV_D.append(3198566.5019376895343388)
+if(platform=="linux2" and uname()[4]=="x86_64"):
+	TruthOutput_Err2_VV_D.append(5798394.23868355906867743)
+else:
+	TruthOutput_Err2_VV_D.append(5798394.2386835580691695)
+		
+TruthOutput_Err2_VV_D.append(8798893.46296149186264514)
+TruthOutput_Err2_VV_D.append(12200064.17477148882412910)
+
+# Truth values for d_spacing_to_tof_focused_det scalar-scalar version
+TruthOutput_SS_D = DoubleVPair()
+TruthOutput_SS_D.val = 1264.17918742388204636
+TruthOutput_SS_D.val_err2 = 999410.25272388576716936
+
+###############################################################################
+# Create output placeholders for vectors
+###############################################################################
+
+# Output placeholders for d_spacing_to_tof_focused_det vector-vector version
+Output_VV_D = DoubleNessiVector(len(Input_D))
+Output_Err2_VV_D = DoubleNessiVector(len(Input_Err2_D))
+
+# Output placeholders for d_spacing_to_tof_focused_det scalar-scalar version
+Output_SS_D = DoubleVPair()
+
+print "Checking Vector-Vector D-Spacing to TOF Focused Detector Binding "\
+      +"Function"
+
+d_spacing_to_tof_focused_det_d(Input_D, Input_Err2_D,
+                               pathlength_focused, pathlength_focused_err2,
+                               polar_angle_focused, polar_angle_focused_err2,
+                               Output_VV_D, Output_Err2_VV_D)
+
+mess = test_common_bind.makeCheck("d_spacing_to_tof_focused_det_d",
+                                  Output_VV_D,
+                                  TruthOutput_VV_D,
+                                  Output_Err2_VV_D,
+                                  TruthOutput_Err2_VV_D)
+print mess
+print
+print "Checking Scalar-Scalar D-Spacing to TOF Focused Detector Binding "\
+      +"Function"
+
+d_spacing_to_tof_focused_det_ss_d(Input_D[0], Input_Err2_D[0],
+                                  pathlength_focused, pathlength_focused_err2,
+                                  polar_angle_focused,
+                                  polar_angle_focused_err2,
+                                  Output_SS_D)
+
+mess = test_common_bind.makeCheck("d_spacing_to_tof_focused_det_d",
+                                  Output_SS_D.val,
+                                  TruthOutput_SS_D.val,
+                                  Output_SS_D.val_err2,
+                                  TruthOutput_SS_D.val_err2)
+
+print mess
+
+##
+# \} // end of test_d_spacing_to_tof_focused_det group
+#
