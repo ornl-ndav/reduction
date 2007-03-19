f1526b88363a2429be8f641b5b276ab6fd384cbf
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2007-03-19 15:46:34 +0000

    Adding PAL implementation and smoke test. This resolves #345.

--- trunk/bindings/test/PAL/runall_pal.sh
+++ trunk/bindings/test/PAL/runall_pal.sh
@@ -15,6 +15,8 @@ python d_spacing_to_tof_focused_det_test.py
 echo
 python div_ncerr_test.py
 echo
+python energy_to_wavelength_test.py
+echo
 python energy_transfer_test.py
 echo
 python frequency_to_angular_frequency_test.py
