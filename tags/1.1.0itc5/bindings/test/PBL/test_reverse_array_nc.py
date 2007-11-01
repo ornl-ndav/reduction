2418f25ca93d26e605e392959a51204af87ca129
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2007-11-01 14:38:43 +0000

    Marked r1430 as milestone:1.1.0itc5

---
+++ tags/1.1.0itc5/bindings/test/PBL/test_reverse_array_nc.py
@@ -0,0 +1,79 @@
+###############################################################################
+#
+# Testing script for Swig bindings for reverse_array_nc functions
+#
+# $Id$
+#
+###############################################################################
+
+##
+# \file bindings/test/PBL/test_reverse_array_nc.py
+#
+
+##
+# \defgroup test_reverse_array_nc pbl::test_reverse_array_nc
+# \{
+#
+
+from axis_manip_bind import reverse_array_nc_d
+from axis_manip_bind import reverse_array_nc_i
+from nessi_vector_bind import *
+import test_common_bind
+
+print "##################################################################"
+print "# Checking Reverse_Array_Nc Swig Generated Python Binding Layer  #"
+print "##################################################################"
+print
+
+# Set standard size for vectors
+NUM_VAL = 5
+
+# Create input vectors
+Input_D = DoubleNessiVector()
+Input_Err2_D = DoubleNessiVector()
+Input_I = IntNessiVector()
+Input_Err2_I = IntNessiVector()
+
+# Place values in the input vectors
+for counter in range(NUM_VAL):
+    Input_D.append(float(counter+1))
+    Input_I.append(counter+1)
+
+###############################################################################
+# Create truth vectors and values
+###############################################################################
+
+# Truth values for reverse_array_nc version
+TruthOutput_D = DoubleNessiVector()
+TruthOutput_D.append(5.0)
+TruthOutput_D.append(4.0)
+TruthOutput_D.append(3.0)
+TruthOutput_D.append(2.0)
+TruthOutput_D.append(1.0)
+
+TruthOutput_I = IntNessiVector()
+TruthOutput_I.append(5)
+TruthOutput_I.append(4)
+TruthOutput_I.append(3)
+TruthOutput_I.append(2)
+TruthOutput_I.append(1)
+
+print "Checking Reverse Array Non-Copy Binding Function"
+
+reverse_array_nc_d(Input_D)
+
+mess = test_common_bind.makeCheck("reverse_array_nc_d",
+                                  Input_D, TruthOutput_D)
+
+print mess
+
+reverse_array_nc_i(Input_I)
+
+mess = test_common_bind.makeCheck("reverse_array_nc_i",
+                                  Input_I, TruthOutput_I)
+
+print mess
+
+##
+# \} // end of test_reverse_array_nc group
+#
