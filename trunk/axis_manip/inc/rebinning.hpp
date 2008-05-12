b05721dcde1137e56ddba45d270467d40a2a61e0
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2008-05-12 17:15:32 +0000

    Adding formulas and tweaking C++ documentation. This refs #451.

--- trunk/axis_manip/inc/rebinning.hpp
+++ trunk/axis_manip/inc/rebinning.hpp
@@ -311,7 +311,35 @@ namespace AxisManip
    * is represented by a histogram model. The fractional overlap is tracked 
    * separately. When processing the data, if either the input value or its 
    * associated error is <em>nan</em> or <em>inf</em>, the bin is ignored and 
-   * the fraction is not added. 
+   * the fraction is not added. <b>NOTE</b>: The data and fractional overlap 
+   * arrays must be divided by a separate function. This function does not 
+   * do that in order to allow investigation of the information.
+   *
+   * For each bin in the output array, the input data, squared uncertainty and 
+   * fractional area are determined in the following manner:
+   *
+   * \f[
+   * data_{out}[k] = \sum_j data_{in}[j] \times \frac{\Delta_{jk}}{\delta_j}
+   * \f]
+   * \f[
+   * \sigma_{out}^2[k] = \sum_j \sigma_{in}^2[j] \times 
+   * \left(\frac{\Delta_{jk}}{\delta_j}\right)^2
+   * \f]
+   * \f[
+   * f[k] = \sum_j \frac{\Delta_{jk}}{\delta_j}
+   * \f] 
+   * 
+   * where \f$k\f$ is the current bin in the output and \f$j\f$ are the bins 
+   * in the input which have overlap with \f$k\f$. The variables 
+   * \f$\Delta_{jk}\f$ and \f$\delta_j\f$ are given by
+   *
+   * \f[
+   * \Delta_{jk} = min(axis_{out}[k+1], axis_{in}[j+1]) - 
+   * max(axis_{out}[k], axis_{in}[j])
+   * \f]
+   * \f[
+   * \delta_j = axis_{in}[j+1] - axis_{in}[j]
+   * \f]
    *
    * \param axis_in (INPUT) is the initial data axis
    * \param input (INPUT) is the data associated with the initial axis
