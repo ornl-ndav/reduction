d918dc82d6dbe71de166c305527f635b0974f792
pah <pah@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2008-06-10 18:49:02 +0000

    checkin frequency_to_angular_frequency.hpp this refs #453

--- branches/Summer2008/axis_manip/src/frequency_to_angular_frequency.hpp
+++ branches/Summer2008/axis_manip/src/frequency_to_angular_frequency.hpp
@@ -88,6 +88,10 @@ namespace AxisManip
     retstr += __frequency_to_angular_frequency_static(a,a2);
 
     size_t sz = frequency.size();
+
+	#pragma omp parallel for default(shared) private(i, retstr) \
+		reduction(+:retstr)
+	{
     for (size_t i=0; i < sz; ++i)
       {
         retstr += __frequency_to_angular_frequency_dynamic(frequency[i],
@@ -97,6 +101,7 @@ namespace AxisManip
                                                            a,
                                                            a2);
       }
+	}
 
     return retstr;
 
