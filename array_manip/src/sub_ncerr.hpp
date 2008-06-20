dd3335138acb4b0305718ab6b16e97ef747fb200
pah <pah@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2008-06-20 15:05:58 +0000

    checkin array_manip/src/sub_ncerr.hpp this refs #452

--- array_manip/src/sub_ncerr.hpp
+++ array_manip/src/sub_ncerr.hpp
@@ -81,16 +81,14 @@ namespace ArrayManip
       }
 
 
-    size_t size = array_in.size();
+    int size = (int) array_in.size();
 
 	#pragma omp parallel for
-	{
-    for (size_t i = 0; i < size; ++i)
-      {
+    for (int i = 0; i < size; i++)
+    {
         array_out[i] = array_in[i] - scalar;
         array_out_err2[i] = array_in_err2[i] + scalar_err2;
-      }
-	}
+    }
     return Nessi::EMPTY_WARN;
   }
 
@@ -134,16 +132,14 @@ namespace ArrayManip
       }
 
 
-    size_t size = array_in.size();
+    int size = (int) array_in.size();
 	
 	#pragma omp parallel for
-	{
-    for (size_t i = 0; i < size; ++i)
-      {
+    for (int i = 0; i < size; i++)
+    {
         array_out[i] = scalar - array_in[i];
         array_out_err2[i] = array_in_err2[i] + scalar_err2;
-      }
-	}
+    }
     return Nessi::EMPTY_WARN;
   }
 
