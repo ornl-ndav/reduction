65080b4b9339e6bdbd1a27571ea3544bed6d84e8
2zr <2zr@ef63e8f5-3107-0410-ae06-bebfbdb344cb>
2008-10-01 13:35:10 +0000

    Changing over to new function name. This refs #475.

--- trunk/axis_manip/src/wavelength_to_velocity.cpp
+++ trunk/axis_manip/src/wavelength_to_velocity.cpp
@@ -25,9 +25,9 @@
 /**
  * $Id$
  *
- * \file axis_manip/src/final_velocity_igs.cpp
+ * \file axis_manip/src/wavelength_to_velocity.cpp
  */
-#include "final_velocity_igs.hpp"
+#include "wavelength_to_velocity.hpp"
 
 namespace AxisManip
 {
@@ -35,25 +35,25 @@ namespace AxisManip
   /**
    * This is the float declaration of the function defined in 3.20.
    *
-   * \ingroup final_velocity_igs
+   * \ingroup wavelength_to_velocity
    */
   template std::string
-  final_velocity_igs<float>(const float analyzer_wavelength,
-                            const float analyzer_wavelength_err2,
-                            float & final_velocity,
-                            float & final_velocity_err2,
-                            void *temp);
+  wavelength_to_velocity<float>(const float analyzer_wavelength,
+                                const float analyzer_wavelength_err2,
+                                float & final_velocity,
+                                float & final_velocity_err2,
+                                void *temp);
 
   /**
    * This is the double precision float declaration of the function
    * defined in 3.20.
    *
-   * \ingroup final_velocity_igs
+   * \ingroup wavelength_to_velocity
    */
   template std::string
-  final_velocity_igs<double>(const double analyzer_wavelength,
+  wavelength_to_velocity<double>(const double analyzer_wavelength,
                              const double analyzer_wavelength_err2,
-                             double & final_velocity,
-                             double & final_velocity_err2,
-                             void *temp);
+                                 double & final_velocity,
+                                 double & final_velocity_err2,
+                                 void *temp);
 } // AxisManip
