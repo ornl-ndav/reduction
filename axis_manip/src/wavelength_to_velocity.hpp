4c4f1727ed1f019e3a214e286e029fcd8c49d768
2zr <mareuternh@gmail.com>
2008-10-01 13:35:10 +0000

    Changing over to new function name. This refs #475.

--- axis_manip/src/wavelength_to_velocity.hpp
+++ axis_manip/src/wavelength_to_velocity.hpp
@@ -25,10 +25,10 @@
 /**
  * $Id$
  *
- * \file axis_manip/src/final_velocity_igs.hpp
+ * \file axis_manip/src/wavelength_to_velocity.hpp
  */
-#ifndef _FINAL_VELOCITY_IGS_HPP
-#define _FINAL_VELOCITY_IGS_HPP 1
+#ifndef _WAVELENGTH_TO_VELOCITY_HPP
+#define _WAVELENGTH_TO_VELOCITY_HPP 1
 
 #include "conversions.hpp"
 #include <stdexcept>
@@ -38,14 +38,14 @@ namespace AxisManip
   // 3.20
   template <typename NumT>
   std::string
-  final_velocity_igs(const NumT analyzer_wavelength,
-                     const NumT analyzer_wavelength_err2,
-                     NumT & final_velocity,
-                     NumT & final_velocity_err2,
-                     void *temp=NULL)
+  wavelength_to_velocity(const NumT analyzer_wavelength,
+                         const NumT analyzer_wavelength_err2,
+                         NumT & final_velocity,
+                         NumT & final_velocity_err2,
+                         void *temp=NULL)
   {
-    throw std::runtime_error("Function [final_velocity_igs] not implemented");
+    throw std::runtime_error("Function [wavelength_to_velocity] not implemented");
   }
 } // AxisManip
 
-#endif // _FINAL_VELOCITY_IGS_HPP
+#endif // _WAVELENGTH_TO_VELOCITY_HPP
